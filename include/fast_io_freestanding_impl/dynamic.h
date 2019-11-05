ad6bb691ed5ebbf121aee30bba8c94bc306ab4bd
ejsvifq_mabmip <euloanty@live.com>
2019-11-05 07:51:51 -0500

    dynamic dispatch

--- include/fast_io_freestanding_impl/dynamic.h
+++ include/fast_io_freestanding_impl/dynamic.h
@@ -11,7 +11,7 @@ public:
 private:
 	struct base
 	{
-		virtual char_type* reads(char_type*,char_type*) = 0;
+		virtual char_type* mmreads(char_type*,char_type*) = 0;
 		virtual ~base() = default;
 	};
 	template<input_stream input>
@@ -20,7 +20,7 @@ private:
 		input in;
 		template<typename ...Args>
 		derv(std::in_place_type_t<input>,Args&& ...args):in(std::forward<Args>(args)...){}
-		char_type* reads(char_type* b,char_type* e) {return reads(in,b,e);}
+		char_type* mmreads(char_type* b,char_type* e) {return reads(in,b,e);}
 	};
 	std::unique_ptr<base> up;
 public:
@@ -32,7 +32,7 @@ public:
 	{
 		char_type *pb(static_cast<char_type*>(static_cast<void*>(std::to_address(b))));
 		char_type *pe(static_cast<char_type*>(static_cast<void*>(std::to_address(e))));
-		return b+(in.up->reads(pb,pe)-pb)*sizeof(*b)/sizeof(char_type);
+		return b+(in.up->mmreads(pb,pe)-pb)*sizeof(*b)/sizeof(char_type);
 	}
 };
 
@@ -44,8 +44,8 @@ public:
 private:
 	struct base
 	{
-		virtual void writes(char_type const*,char_type const*) = 0;
-		virtual void flush() = 0;
+		virtual void mmwrites(char_type const*,char_type const*) = 0;
+		virtual void mmflush() = 0;
 		virtual ~base() = default;
 	};
 	template<output_stream output>
@@ -54,8 +54,8 @@ private:
 		output out;
 		template<typename ...Args>
 		derv(std::in_place_type_t<output>,Args&& ...args):out(std::forward<Args>(args)...){}
-		void writes(char_type const* b,char_type const* e) {writes(out,b,e);}
-		void flush() {flush(out);}
+		void mmwrites(char_type const* b,char_type const* e) {writes(out,b,e);}
+		void mmflush() {flush(out);}
 	};
 	std::unique_ptr<base> up;
 public:
@@ -65,10 +65,10 @@ public:
 	template<std::contiguous_iterator Iter>
 	inline friend void writes(basic_dynamic_output_stream& out,Iter b,Iter e)
 	{
-		out.up->writes(static_cast<char_type const*>(static_cast<void const*>(std::to_address(b))),
+		out.up->mmwrites(static_cast<char_type const*>(static_cast<void const*>(std::to_address(b))),
 							static_cast<char_type const*>(static_cast<void const*>(std::to_address(e))));
 	}
-	inline friend void flush(basic_dynamic_output_stream& out) { return out.up->flush();}
+	inline friend void flush(basic_dynamic_output_stream& out) { return out.up->mmflush();}
 };
 
 
@@ -80,9 +80,9 @@ public:
 private:
 	struct base
 	{
-		virtual char_type* reads(char_type*,char_type*) = 0;
-		virtual void writes(char_type const*,char_type const*) = 0;
-		virtual void flush() = 0;
+		virtual char_type* mmreads(char_type*,char_type*) = 0;
+		virtual void mmwrites(char_type const*,char_type const*) = 0;
+		virtual void mmflush() = 0;
 		virtual ~base() = default;
 	};
 	template<io_stream in_output>
@@ -91,9 +91,9 @@ private:
 		in_output io;
 		template<typename ...Args>
 		derv(std::in_place_type_t<in_output>,Args&& ...args):io(std::forward<Args>(args)...){}
-		char_type* reads(char_type* b,char_type* e) {return reads(io,b,e);}
-		void writes(char_type const* b,char_type const* e) {writes(io,b,e);}
-		void flush() {flush(io);}
+		char_type* mmreads(char_type* b,char_type* e) {return reads(io,b,e);}
+		void mmwrites(char_type const* b,char_type const* e) {writes(io,b,e);}
+		void mmflush() {flush(io);}
 	};
 	std::unique_ptr<base> up;
 public:
@@ -105,15 +105,15 @@ public:
 	{
 		char_type *pb(static_cast<char_type*>(static_cast<void*>(std::to_address(b))));
 		char_type *pe(static_cast<char_type*>(static_cast<void*>(std::to_address(e))));
-		return b+(io.up->reads(pb,pe)-pb)*sizeof(*b)/sizeof(char_type);
+		return b+(io.up->mmreads(pb,pe)-pb)*sizeof(*b)/sizeof(char_type);
 	}
 	template<std::contiguous_iterator Iter>
 	inline friend void writes(basic_dynamic_io_stream& io,Iter b,Iter e)
 	{
-		io.up->writes(static_cast<char_type const*>(static_cast<void const*>(std::to_address(b))),
+		io.up->mmwrites(static_cast<char_type const*>(static_cast<void const*>(std::to_address(b))),
 							static_cast<char_type const*>(static_cast<void const*>(std::to_address(e))));
 	}
-	inline friend void flush(basic_dynamic_io_stream& io) { return io.up->flush();}
+	inline friend void flush(basic_dynamic_io_stream& io) { return io.up->mmflush();}
 };
 
 
