59c80395f9136b44af07d8923ac2f9596e66325d
ejsvifq mabmip <euloanty@live.com>
2019-11-12 15:55:33 -0500

    generate offset success

--- examples/0038.ryu/generate_fixed_table.cc
+++ examples/0038.ryu/generate_fixed_table.cc
@@ -1,29 +1,34 @@
 #include"../../include/fast_io.h"
 
-inline constexpr std::uint32_t length_for_index(std::size_t idx) {
+inline constexpr std::uint16_t length_for_index(std::uint64_t idx)
+{
 // [log_10(2^i)] = ((16 * i) * 78913L) >> 18 <-- floor
 // +1 for ceil, +16 for mantissa, +8 to round up when dividing by 9
-return (static_cast<std::uint32_t>(((16 * idx) * 1292913986ul) >> 32) + 25) / 9;
+	return ((((16 * idx) * 1292913986) >> 32) + 25) / 9;
 }
 
 inline constexpr std::size_t length_for_index2(std::size_t idx)
 {
-	return (120*idx)/64+3;
+	return ((120*idx)>>6)+3;
 }
 
 
 inline constexpr auto generate_fixed_offset_table()
 {
-	inline constexpr std::size_t table_size(64);
+	constexpr std::size_t table_size(64);
 	std::array<std::uint16_t,table_size> offset{};
-	std::uint32_t sum(0);
+	std::uint16_t sum(0);
 	for(std::size_t i(0);i!=table_size;++i)
 	{
-		length_for_index(i)
+		offset[i]=sum;
+		sum+=length_for_index(i);
 	}
+	return offset;
 }
 
 int main()
 {
-	
+	constexpr auto fixed_table(generate_fixed_offset_table());
+	for(auto const & e : fixed_table)
+			println(fast_io::out,e);
 }
