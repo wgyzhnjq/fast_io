2236f9ddd0f68d0c0812c300a4aaa979cf80af18
ejsvifq_mabmip <euloanty@live.com>
2019-11-17 00:16:04 -0500

    ryu fixed

--- examples/0009.floating point/double_default.cc
+++ examples/0009.floating point/double_default.cc
@@ -1,13 +1,10 @@
 #include<iostream>
 #include"../../include/fast_io.h"
 #include"ryu.h"
+#include<iomanip>
 int main()
 {
-	constexpr double v(0.000000000000000012393542857386751165905);
-	std::cout<<v<<std::endl;
-	print(fast_io::out,"fast_io:",v,"\nryu source:");
-	std::array<char,1024> arr;
-	auto p(arr.data()+d2s_buffered_n(v,arr.data()));
-	*p='\n';
-	writes(fast_io::out,arr.data(),++p);
+//	constexpr double v(123935428573867511659.05);
+	fprintf(stdout,"%g\n",v);
+	print(fast_io::out,"fast_io:",v);
 }
