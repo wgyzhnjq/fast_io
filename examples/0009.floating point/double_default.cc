1aa213648f3ec6474114432881fdccaa8994fb51
ejsvifq_mabmip <euloanty@live.com>
2019-11-17 01:57:32 -0500

    default

--- examples/0009.floating point/double_default.cc
+++ examples/0009.floating point/double_default.cc
@@ -1,9 +1,8 @@
-#include<iostream>
 #include"../../include/fast_io.h"
-#include"ryu.h"
-#include<iomanip>
 int main()
 {
-	constexpr double v(123935428.57386751165905);
-	println(fast_io::out,"fast_io:",v);
+	println(fast_io::out,123935428.57386751165905);
+	println(fast_io::out,0);
+	println(fast_io::out,1e11);
+	println(fast_io::out,-1e11);
 }
