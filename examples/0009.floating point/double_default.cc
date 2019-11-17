71ad20520782bab7b654014fab7005e61552ed74
ejsvifq_mabmip <euloanty@live.com>
2019-11-17 00:39:45 -0500

    shortest shortest fixed shortest scientific shortest

--- examples/0009.floating point/double_default.cc
+++ examples/0009.floating point/double_default.cc
@@ -4,7 +4,6 @@
 #include<iomanip>
 int main()
 {
-//	constexpr double v(123935428573867511659.05);
-	fprintf(stdout,"%g\n",v);
+	constexpr double v(123935428.57386751165905);
 	print(fast_io::out,"fast_io:",v);
 }
