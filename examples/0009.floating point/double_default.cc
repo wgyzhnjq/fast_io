2553b692a294af916397bd205afb68ff0e39065c
ejsvifq_mabmip <euloanty@live.com>
2019-11-17 01:42:05 -0500

    bug fixed for small number

--- examples/0009.floating point/double_default.cc
+++ examples/0009.floating point/double_default.cc
@@ -4,6 +4,6 @@
 #include<iomanip>
 int main()
 {
-	constexpr double v(123935428.57386751165905);
-	print(fast_io::out,"fast_io:",v);
+	constexpr double v(99999997952);//(123935428.57386751165905);
+	println(fast_io::out,"fast_io:",v);
 }
