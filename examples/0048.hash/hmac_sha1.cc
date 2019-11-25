562b2d42577fc9da0845cc791a75e114ea623dad
ejsvifq_mabmip <euloanty@live.com>
2019-11-25 08:29:09 -0500

    deprecated

--- examples/0048.hash/hmac_sha1.cc
+++ examples/0048.hash/hmac_sha1.cc
@@ -5,7 +5,7 @@
 int main()
 try
 {
-	fast_io::hmac_sha1 hmacs1("12345");
+	fast_io::hmac_sha1 hmacs1("123452351532319875129385712389547192834791832ru89231ur98217r9123yr29813ry89123ry89123yr98123ry132r12");
 	print(hmacs1,"abc");
 	flush(hmacs1);
 	println(fast_io::out,hmacs1);
