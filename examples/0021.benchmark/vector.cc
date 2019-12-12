7a67d702498f8f0a584ffc424493cbc7bcdb4868
ejsvifq_mabmip <euloanty@live.com>
2019-12-12 13:09:57 -0500

    vec

--- examples/0021.benchmark/vector.cc
+++ examples/0021.benchmark/vector.cc
@@ -8,26 +8,16 @@ int main()
 	cqw::timer tm("vector");
 	std::vector<std::size_t> vec;
 	vec.reserve(N);
-	for(std::size_t i(0);i<N;i+=4)
-	{
+	for(std::size_t i(0);i!=N;++i)
 		vec.emplace_back(i);
-		vec.emplace_back(i+1);
-		vec.emplace_back(i+2);
-		vec.emplace_back(i+3);
-	}
 
 }
 {
 	cqw::timer tm("vector emplace_back_unchecked");
 	std::vector<std::size_t> vec;
 	vec.reserve(N);
-	for(std::size_t i(0);i<N;i+=4)
-	{
+	for(std::size_t i(0);i!=N;++i)
 		vec.emplace_back_unchecked(i);
-		vec.emplace_back_unchecked(i+1);
-		vec.emplace_back_unchecked(i+2);
-		vec.emplace_back_unchecked(i+3);
-	}
 }
 
 
