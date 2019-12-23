cc9403d5ae50195ee266849394914c5725f4fa29
expnkx <unlvsur@live.com>
2019-12-23 00:41:37 -0500

    C style io handle

--- examples/0059.legacy/cstyle.cc
+++ examples/0059.legacy/cstyle.cc
@@ -2,7 +2,9 @@
 
 int main()
 {
+	fprintf(fast_io::c_stdout,"%d\n",6);		//call standard function but checking error
 	println(fast_io::c_stdout,"Hello World");		// call fast_io's println
-	auto posix_handle(static_cast<fast_io::posix_io_handle>(fast_io::c_stdout));
-	println(posix_handle,u8"dsgsdg\n");
+//cast c_style_io_handle to posix handle for c_stdout. Negative effect for coding in a EBIDIC system. DON'T DO THIS IF YOU DON'T NEED IT
+	fast_io::posix_io_handle posix_handle(fast_io::c_stdout);
+	println(posix_handle,u8"dsgsdg\n");//posix_handle is utf-8 coding
 }
--- include/fast_io_legacy_impl/c_style.h
+++ include/fast_io_legacy_impl/c_style.h
@@ -33,6 +33,53 @@ public:
 #endif
 };
 
+
+
+template<typename... Args>
+requires requires(std::FILE* fp,Args&& ...args)
+{
+	std::fprintf(fp,std::forward<Args>(args)...);
+}
+inline auto fprintf(c_style_io_handle_unlocked& h,Args&& ...args)
+{
+	auto v(
+#if defined(__WINNT__) || defined(_MSC_VER)
+_fprintf_nolock
+#elif defined(_POSIX_SOURCE)
+	fprintf_unlocked
+#else
+	fprintf
+#endif
+(h.native_handle(),std::forward<Args>(args)...));
+// forgetting checking printf/fprintf return value is a common mistake and a huge source of security vulnerability
+	if(v<0)
+		throw std::system_error(errno,std::system_category());
+	return v;
+}
+
+
+template<typename... Args>
+requires requires(std::FILE* fp,Args&& ...args)
+{
+	std::fscanf(fp,std::forward<Args>(args)...);
+}
+inline auto fscanf(c_style_io_handle_unlocked& h,Args&& ...args)
+{
+	auto const v(
+#if defined(__WINNT__) || defined(_MSC_VER)
+_fscanf_nolock
+#elif defined(_POSIX_SOURCE)
+	fscanf_unlocked
+#else
+	fscanf
+#endif
+(h.native_handle(),std::forward<Args>(args)...));
+// forgetting checking scanf/fscanf return value is a common mistake and a huge source of security vulnerability
+	if(v<0)
+		throw std::system_error(errno,std::system_category());
+	return v;
+}
+
 /*
 #ifdef _MSC_VER
 
@@ -397,6 +444,36 @@ public:
 		close_impl();
 	}
 };
+
+template<typename... Args>
+requires requires(std::FILE* fp,Args&& ...args)
+{
+	std::fprintf(fp,std::forward<Args>(args)...);
+}
+inline auto fprintf(c_style_io_handle& h,Args&& ...args)
+{
+	auto v(std::fprintf(h.native_handle(),std::forward<Args>(args)...));
+// forgetting checking printf/fprintf return value is a common mistake and a huge source of security vulnerability
+	if(v<0)
+		throw std::system_error(errno,std::system_category());
+	return v;
+}
+
+
+template<typename... Args>
+requires requires(std::FILE* fp,Args&& ...args)
+{
+	std::fscanf(fp,std::forward<Args>(args)...);
+}
+inline auto fscanf(c_style_io_handle& h,Args&& ...args)
+{
+	auto v(std::fscanf(h.native_handle(),std::forward<Args>(args)...));
+// forgetting checking scanf/fscanf return value is a common mistake and a huge source of security vulnerability
+	if(v<0)
+		throw std::system_error(errno,std::system_category());
+	return v;
+}
+
 
 using c_style_file = basic_c_style_file<c_style_io_handle>;
 using c_style_file_unlocked = basic_c_style_file<c_style_io_handle_unlocked>;
