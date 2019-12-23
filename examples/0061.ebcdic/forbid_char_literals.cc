9b04097c875503ea6f9e832fc3de627c120f7150
expnkx <unlvsur@live.com>
2019-12-23 04:58:42 -0500

    error message shorten with static_assert

--- examples/0061.ebcdic/forbid_char_literals.cc
+++ examples/0061.ebcdic/forbid_char_literals.cc
@@ -3,7 +3,7 @@
 int main(int argc,char** argv)
 {
 	println(fast_io::out,u8"u8device");//pass
-	println(fast_io::out,fast_io::manip::from_utf8_unchecked(*argv));
+	println(fast_io::out,fast_io::from_utf8_unchecked(*argv));
 //deal with char* to u8 strings
 //learn from Rust*argv);//pass
 	println(fast_io::out,"device");//error
