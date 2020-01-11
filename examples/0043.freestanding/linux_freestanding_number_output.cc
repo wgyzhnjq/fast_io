d34d9ec7be785b94cc894a160f3c4c571d2e94ac
expnkx <unlvsur@live.com>
2020-01-11 13:16:36 -0500

    wtf??

--- examples/0043.freestanding/linux_freestanding_number_output.cc
+++ examples/0043.freestanding/linux_freestanding_number_output.cc
@@ -1,4 +1,4 @@
-#include"../../include/fast_io_core.h"
+#include"../../include/fast_io.h"
 
 //Compilation success
 //g++ -o linux_freestanding_number_output linux_freestanding_number_output.cc -O3 -std=c++2a -fconcepts -nostartfiles -ffreestanding -fno-exceptions -fno-rtti
@@ -8,12 +8,7 @@ extern "C"
 {
 void _start() noexcept
 {
-	std::array<char,10000> arr;
-	fast_io::ospan out(arr);
-	println(out,u8"asdhasdhidasogh120asfas\n");
-	auto isv(to_istring_view(out));
-	int value(5);
-	scan(isv,value);
-	_Exit(value);
+	println(23451235);
+	fast_io::fast_exit(0);
 }
 }
