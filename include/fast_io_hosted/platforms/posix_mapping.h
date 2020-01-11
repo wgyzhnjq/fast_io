515bab07f9fea66faa551a0482d1a2104f6dcbbc
expnkx <unlvsur@live.com>
2020-01-11 10:44:03 -0500

    fno-exceptions

--- include/fast_io_hosted/platforms/posix_mapping.h
+++ include/fast_io_hosted/platforms/posix_mapping.h
@@ -53,7 +53,11 @@ inline constexpr posix_file_map_attribute to_posix_file_map_attribute(file_map_a
 	case file_map_attribute::read_write:return posix_file_map_attribute::read|posix_file_map_attribute::write;
 	case file_map_attribute::write_copy:return posix_file_map_attribute::write;
 	default:
+#ifdef __cpp_exceptions
 		throw std::runtime_error("unknown file_mapping_attribute");
+#else
+		std::terminate();
+#endif
 	};
 }
 
