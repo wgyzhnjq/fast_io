e2b7050c7b8695299c7f9b3266a74c8c1ce43046
expnkx <unlvsur@live.com>
2020-01-11 13:21:36 -0500

    ryu freef

--- examples/0043.freestanding/linux_freestanding_number_output.cc
+++ examples/0043.freestanding/linux_freestanding_number_output.cc
@@ -1,14 +1,15 @@
 #include"../../include/fast_io.h"
 
 //Compilation success
-//g++ -o linux_freestanding_number_output linux_freestanding_number_output.cc -O3 -std=c++2a -fconcepts -nostartfiles -ffreestanding -fno-exceptions -fno-rtti
-//Execuation:./linux_freestanding_number_output; echo $?
+//g++ -o linux_freestanding_number_output linux_freestanding_number_output.cc  -Ofast -std=c++2a -s -fno-exceptions -fno-rtti -ffreestanding -nostartfile
+//Execuation:./linux_freestanding_number_output
+//ryu algorithm
 
 extern "C"
 {
 void _start() noexcept
 {
-	println(23451235);
+	println(23451235.12421);
 	fast_io::fast_exit(0);
 }
 }
