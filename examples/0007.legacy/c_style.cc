38b22c54cd04ed032d6ef58c58ae8e9896b6e8b7
expnkx <unlvsur@live.com>
2020-01-11 18:36:15 -0500

    lgc

--- examples/0007.legacy/c_style.cc
+++ examples/0007.legacy/c_style.cc
@@ -5,5 +5,5 @@ int main()
 	fprintf(fast_io::c_stdout,"%d\n",6);		//call standard function but checking error
 	println(fast_io::c_stdout,u8"Hello World");		// call fast_io's println
 	fast_io::posix_io_handle posix_handle(fast_io::c_stdout);	//gets its internal posix handle
-	println(posix_handle,u8"dsgsdg\n");
+	println(posix_handle,u8"dsgsdg");
 }
