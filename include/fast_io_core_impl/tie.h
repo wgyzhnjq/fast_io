8c7249e24e518c0b724c1f07436eab42676b526f
expnkx <unlvsur@live.com>
2020-01-12 15:33:14 -0500

    t

--- include/fast_io_core_impl/tie.h
+++ include/fast_io_core_impl/tie.h
@@ -35,10 +35,10 @@ inline constexpr void put(tie<T,out>& t,typename T::char_type ch)
 }
 
 template<input_stream T,output_stream O,std::contiguous_iterator Iter>
-inline constexpr Iter receive(tie<T,O>& t,Iter begin,Iter end)
+inline constexpr Iter read(tie<T,O>& t,Iter begin,Iter end)
 {
 	flush(t.to());
-	return receive(t.native_handle(),begin,end);
+	return read(t.native_handle(),begin,end);
 }
 
 template<output_stream T,output_stream out>
@@ -49,10 +49,10 @@ inline constexpr void flush(tie<T,out>& t)
 }
 
 template<output_stream T,output_stream out,std::contiguous_iterator Iter>
-inline constexpr auto send(tie<T,out>& t,Iter begin,Iter end)
+inline constexpr auto write(tie<T,out>& t,Iter begin,Iter end)
 {
 	flush(t.to());
-	return send(t.native_handle(),begin,end);
+	return write(t.native_handle(),begin,end);
 }
 
 template<character_output_stream T,output_stream out>
@@ -89,14 +89,7 @@ inline constexpr void flush(self_tie<T>& t)
 {
 	flush(t.native_handle());
 }
-/*
-template<bool err=false,character_input_stream T>
-inline constexpr auto get(self_tie<T>& t)
-{
-	flush(t);
-	return get<err>(t.native_handle());
-}
-*/
+
 template<character_output_stream T>
 inline constexpr auto put(self_tie<T>& t,typename T::char_type ch)
 {
@@ -105,21 +98,21 @@ inline constexpr auto put(self_tie<T>& t,typename T::char_type ch)
 }
 
 template<input_stream T,std::contiguous_iterator Iter>
-inline constexpr Iter receive(self_tie<T>& t,Iter begin,Iter end)
+inline constexpr Iter read(self_tie<T>& t,Iter begin,Iter end)
 {
 	flush(t);
-	return receive(t.native_handle(),begin,end);
+	return read(t.native_handle(),begin,end);
 }
 
 template<output_stream T,std::contiguous_iterator Iter>
-inline constexpr auto send(self_tie<T>& t,Iter begin,Iter end)
+inline constexpr auto write(self_tie<T>& t,Iter begin,Iter end)
 {
 	flush(t);
-	return send(t.native_handle(),begin,end);
+	return write(t.native_handle(),begin,end);
 }
 
 template<character_output_stream T>
-inline constexpr void fill_nc(self_tie<T>& t,std::size_t count,typename T::char_type const& ch)
+inline constexpr void fill_nc(self_tie<T>& t,std::size_t count,typename T::char_type ch)
 {
 	flush(t);
 	fill_nc(t.native_handle(),count,ch);
