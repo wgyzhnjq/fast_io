8744920bb4e1105d25120dac8d977e16fc30b7f1
expnkx <unlvsur@live.com>
2020-01-13 00:53:24 -0500

    trans

--- benchmarks/0002.transmit/transmit.cc
+++ benchmarks/0002.transmit/transmit.cc
@@ -8,22 +8,28 @@ int main()
 try
 {
 	{
-		fast_io::timer tm("fast_io ofstream <= ifstream");
+		fast_io::timer tm("ofstream <= ifstream");
 		std::ifstream ifst("large_file.txt",std::ifstream::binary);
 		std::ofstream ofst("large_file_stream.txt",std::ofstream::binary);
 		ofst<<ifst.rdbuf();
 	}
 	{
-		fast_io::timer tm("fast_io obuf_file <= ibuf_file");
+		fast_io::timer tm("obuf_file <= ibuf_file");
 		fast_io::ibuf_file ib("large_file.txt");
 		fast_io::obuf_file ob("large_file_ibuf_to_obuf.txt");
 		transmit(ob,ib);
 	}
 	{
-		fast_io::timer tm("fast_io onative_file <= inative_file");
+		fast_io::timer tm("onative_file <= inative_file");
 		fast_io::inative_file ib("large_file.txt");
 		fast_io::onative_file ob("large_file_inative_file_to_onative_file.txt");
 		transmit(ob,ib);
+	}
+	{
+		fast_io::timer tm("c_style_file <= ibuf_file");
+		fast_io::ibuf_file ib("large_file.txt");
+		fast_io::c_style_file csf("large_file_ibuf_to_c_style_file.txt","wb");
+		transmit(csf,ib);
 	}
 }
 catch(std::exception const& e)
