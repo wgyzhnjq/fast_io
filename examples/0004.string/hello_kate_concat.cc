4dc6b9e394bdcaf2f3d33a74903489bb0d478e69
expnkx <unlvsur@live.com>
2020-01-12 20:38:50 -0500

    hello kate

---
+++ examples/0004.string/hello_kate_concat.cc
@@ -0,0 +1,14 @@
+//CppCon 2015: Kate Gregory “Stop Teaching C"
+//https://www.youtube.com/watch?v=YnWhqhNdYyk&t=744s
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_in.h"
+
+int main()
+{
+	std::string name;
+	scan(name);
+	std::string greeting = fast_io::concat("Hello, " , name);
+	if (name == "Kate")
+		greeting += ", I know you!";
+	println(greeting);
+}
