5c74eb900e5618c99c4eb90432f3e7bd05614be5
expnkx <unlvsur@live.com>
2020-01-12 21:49:43 -0500

    transmit

--- examples/0009.transmit/transmit.cc
+++ examples/0009.transmit/transmit.cc
@@ -1,12 +1,12 @@
-#include"../../include/fast_io.h"
+#include"../../include/fast_io_in.h"
 #include"../../include/fast_io_legacy.h"
 
 int main()
 {
-	transmit(fast_io::out,fast_io::c_stdin);
+	transmit(fast_io::c_stdout,fast_io::in);
 }
 /*
-Transmit from stdin to fast_io::out
+Transmit from fast_io::in to C stdout
 
 Should be zero copy IO on linux
 */
