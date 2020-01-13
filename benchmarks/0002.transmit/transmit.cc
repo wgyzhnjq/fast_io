494b6ec3576972acd369b143043c24f571e76121
expnkx <unlvsur@live.com>
2020-01-13 00:34:00 -0500

    transmit

--- benchmarks/0002.transmit/transmit.cc
+++ benchmarks/0002.transmit/transmit.cc
@@ -16,10 +16,15 @@ try
 	{
 		fast_io::timer tm("fast_io obuf_file <= ibuf_file");
 		fast_io::ibuf_file ib("large_file.txt");
-		fast_io::obuf_file ob("large_file_fast_io.txt");
+		fast_io::obuf_file ob("large_file_ibuf_to_obuf.txt");
 		transmit(ob,ib);
 	}
-
+	{
+		fast_io::timer tm("fast_io obuf_file <= ibuf_file");
+		fast_io::inative_file ib("large_file.txt");
+		fast_io::onative_file ob("large_file_inative_file_to_onative_file.txt");
+		transmit(ob,ib);
+	}
 }
 catch(std::exception const& e)
 {
