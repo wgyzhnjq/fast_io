3fa8ba6df83a93e23dea58a1edd0bd90d6720a89
expnkx <unlvsur@live.com>
2020-01-16 01:50:41 -0500

    sha

---
+++ include/fast_io_hash/sha.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace fast_io
+{
+
+template<typename T>
+struct sha
+{
+public:
+	using char_type = char;
+	using function_type = T;
+	typename T::digest_type digest = T::digest_initial_value;
+	typename T::block_type block{};
+	T function;
+	std::size_t transform_counter{};
+	template<output_stream output, std::contiguous_iterator Iter>
+	inline constexpr Iter write_proxy(output&, Iter begin, Iter end)
+	{
+		std::span be(std::as_bytes(std::span(begin,end)));
+		for(;block.size()<=be.size();++transform_counter)
+		{
+			function(std::span(digest),std::span<std::byte,64>(block));
+			be=subspan(block.size());
+		}
+		return e - be.size() / sizeof(*b);
+	}
+};
+
+
+}
