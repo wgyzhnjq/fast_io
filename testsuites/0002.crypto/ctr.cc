1b03e50d853851d7ffda9533fca6473fc27bc778
zyddnys <zyddnys@outlook.com>
2020-01-16 23:13:20 -0500

    fix ctr

--- testsuites/0002.crypto/ctr.cc
+++ testsuites/0002.crypto/ctr.cc
@@ -13,7 +13,19 @@ int main()
         print(ob,"hello world\nxxxx\n8988\n");
     }
     {
-        fast_io::crypto::ictr_little_endian<fast_io::ibuf_file, fast_io::crypto::speck::speck_dec_128_128> 
+        fast_io::crypto::ictr_little_endian<fast_io::ibuf_file, fast_io::crypto::speck::speck_enc_128_128> 
+            ib(std::piecewise_construct,std::forward_as_tuple("text.txt"),
+            std::forward_as_tuple(std::as_bytes(std::span(key)), std::as_bytes(std::span(nonce))));
+        transmit(fast_io::out,ib);
+    }
+    {
+        fast_io::crypto::octr_big_endian<fast_io::obuf_file, fast_io::crypto::speck::speck_enc_128_128> 
+            ob(std::piecewise_construct,std::forward_as_tuple("text.txt"),
+            std::forward_as_tuple(std::as_bytes(std::span(key)), std::as_bytes(std::span(nonce))));
+        print(ob,"hello world\nxxxx\n8988\n");
+    }
+    {
+        fast_io::crypto::ictr_big_endian<fast_io::ibuf_file, fast_io::crypto::speck::speck_enc_128_128> 
             ib(std::piecewise_construct,std::forward_as_tuple("text.txt"),
             std::forward_as_tuple(std::as_bytes(std::span(key)), std::as_bytes(std::span(nonce))));
         transmit(fast_io::out,ib);
