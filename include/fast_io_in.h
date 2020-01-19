b68075f115df320e1b76cbd56f3a65eba1b0b15e
expnkx <unlvsur@live.com>
2020-01-19 18:00:14 -0500

    debug for fast_io::in

--- include/fast_io_in.h
+++ include/fast_io_in.h
@@ -21,3 +21,17 @@ inline constexpr auto scan(T&& t,Args&& ...args)
 {
 	return scan<report_eof>(fast_io::in,std::forward<T>(t),std::forward<Args>(args)...);
 }
+
+#ifndef NDEBUG
+
+template<bool report_eof=false,typename T,typename... Args>
+requires (!fast_io::input_stream<std::remove_cvref_t<T>>)&&requires(T&& t,Args&& ...args)
+{
+	scan<report_eof>(fast_io::in,std::forward<T>(t),std::forward<Args>(args)...);
+}
+inline constexpr auto debug_scan(T&& t,Args&& ...args)
+{
+	return scan<report_eof>(fast_io::in,std::forward<T>(t),std::forward<Args>(args)...);
+}
+
+#endif
