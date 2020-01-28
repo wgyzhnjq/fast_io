0223e2b57938c6c0816a914be79896bf54806057
expnkx <euloanty@live.com>
2020-01-28 00:42:47 -0500

    c style from example more

--- examples/0007.legacy/c_style_from_win32.cc
+++ examples/0007.legacy/c_style_from_win32.cc
@@ -5,5 +5,6 @@
 int main()
 {
 	fast_io::c_style_file csf(fast_io::win32_stdout(),fast_io::open::interface<fast_io::open::out|fast_io::open::binary>);
-	print(csf,"Hello World\n");
+	print(csf,"Hello World from fast_io::print\n");
+	fprintf(csf,"Hello World from std::fprintf\n");
 }
