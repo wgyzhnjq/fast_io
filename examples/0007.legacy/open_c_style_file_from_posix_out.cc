65aa17afd16143ef780d3f6a4e74373a31029468
expnkx <euloanty@live.com>
2020-01-27 22:44:01 -0500

    unlocked

--- examples/0007.legacy/open_c_style_file_from_posix_out.cc
+++ examples/0007.legacy/open_c_style_file_from_posix_out.cc
@@ -3,7 +3,7 @@
 int main()
 {
 	fast_io::posix_io_handle posix_out(1);	//stdout number
-	fast_io::c_style_file csf(std::move(posix_out),"wb");
+	fast_io::c_style_file csf(std::move(posix_out),"wb");	//call fdopen. Must use std::move since c style file will steal resource from posix handle
 	print(csf,"Hello World from fast_io::print\n");
 	fprintf(csf,"Hello World from std::fprintf\n");
 }
