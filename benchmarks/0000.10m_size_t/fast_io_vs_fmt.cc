b9d3541d476205f3de118c2d23ec82a133d231e4
expnkx <euloanty@live.com>
2020-02-04 20:24:27 -0500

    add ostring testsuite

--- benchmarks/0000.10m_size_t/fast_io_vs_fmt.cc
+++ benchmarks/0000.10m_size_t/fast_io_vs_fmt.cc
@@ -4,6 +4,17 @@
 int main()
 {
 	constexpr std::size_t N(10000000);
+	std::size_t fast_io_ostr_value{};
+	{
+	fast_io::timer t("fast_io::ostring");
+	fast_io::ostring ostr;
+	for(std::size_t i{};i!=N;++i)
+	{
+		print(ostr,i);
+		ostr.clear();
+		fast_io_value+=ostr.str().size();
+	}
+	}
 	std::size_t fast_io_value{};
 	{
 	fast_io::timer t("fast_io::to");
@@ -16,7 +27,5 @@ int main()
 	for(std::size_t i{};i!=N;++i)
 		fmt_value+=fmt::format("{}",i).size();
 	}
-
-	println("fast_io::to value:",fast_io_value,"\nfmt::format value:",fmt_value);
-
+	println("fast_io::ostring value:",fast_io_ostr_value,"\nfast_io::to value:",fast_io_value,"\nfmt::format value:",fmt_value);
 }
