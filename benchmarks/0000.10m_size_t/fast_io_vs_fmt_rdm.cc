a84d731ddab004b352d60eb2ad86a320e63d418d
expnkx <euloanty@live.com>
2020-02-05 01:59:40 -0500

    rdm

--- benchmarks/0000.10m_size_t/fast_io_vs_fmt_rdm.cc
+++ benchmarks/0000.10m_size_t/fast_io_vs_fmt_rdm.cc
@@ -1,32 +1,38 @@
 #include"../timer.h"
 #include<fmt/format.h>
+#include<random>
 
 int main()
 {
 	constexpr std::size_t N(10000000);
+	std::vector<std::size_t> vec;
+	vec.reserve(N);
+	std::random_device device;
+	std::mt19937_64 eng(device());
+	std::uniform_int_distribution<std::size_t> dis(0,std::numeric_limits<std::size_t>::max());
+	for(std::size_t i(0);i!=N;++i)
+		vec.emplace_back(dis(eng));
 	std::size_t fast_io_ostr_value{};
 	{
 	fast_io::timer t("fast_io::ostring");
-	fast_io::ostring ostr;
-	ostr.str().reserve(42);
 	for(std::size_t i{};i!=N;++i)
 	{
-		print(ostr,i);
+		fast_io::ostring ostr;
+		print(ostr,vec[i]);
 		fast_io_ostr_value+=ostr.str().size();
-		ostr.clear();
 	}
 	}
 	std::size_t fast_io_value{};
 	{
 	fast_io::timer t("fast_io::concat");
 	for(std::size_t i{};i!=N;++i)
-		fast_io_value+=fast_io::concat(i).size();
+		fast_io_value+=fast_io::concat(vec[i]).size();
 	}
 	std::size_t fmt_value{};
 	{
 	fast_io::timer t("fmt::format");
 	for(std::size_t i{};i!=N;++i)
-		fmt_value+=fmt::format_int(i).str().size();
+		fmt_value+=fmt::format_int(vec[i]).str().size();
 	}
 	println("fast_io::ostring value:",fast_io_ostr_value,"\nfast_io::concat value:",fast_io_value,"\nfmt::format value:",fmt_value);
 }
