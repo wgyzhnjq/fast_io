59f888b5baca8a2ff7eabf4b614dbe28813abd6f
expnkx <euloanty@live.com>
2020-02-08 00:47:35 -0500

    mov???

--- include/fast_io_core_impl/linux/amd64.h
+++ include/fast_io_core_impl/linux/amd64.h
@@ -90,7 +90,7 @@ inline auto system_call(auto p1, auto p2, auto p3, auto p4)
 	std::uint64_t r10 __asm__("r10") = (std::uint64_t)p4;
 	__asm__ __volatile__
 	(
-	"mov %5, %%r10;"
+//	"mov %5, %%r10;"
 	"syscall"
 	: "=a" (ret)
 	//EDI      RSI       RDX
