1d1f7255cca11b58ec66f8303c41d60489bc838d
expnkx <euloanty@live.com>
2020-02-08 00:50:47 -0500

    amd64 all syscalls

--- include/fast_io_core_impl/linux/amd64.h
+++ include/fast_io_core_impl/linux/amd64.h
@@ -87,15 +87,14 @@ requires (1<sizeof(return_value_type))
 inline auto system_call(auto p1, auto p2, auto p3, auto p4)
 {
 	return_value_type ret;
-	std::uint64_t r10 __asm__("r10") = (std::uint64_t)p4;
 	__asm__ __volatile__
 	(
-//	"mov %5, %%r10;"
+	"mov %5, %%r10;"
 	"syscall"
 	: "=a" (ret)
 	//EDI      RSI       RDX
-	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"(r10)
-	: "rcx", "r11", "memory", "cc"
+	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"((std::uint64_t)p4)
+	: "rcx", "r11", "r10", "memory", "cc"
 	);
 	return ret;
 }
@@ -105,15 +104,15 @@ requires (1<sizeof(return_value_type))
 inline auto system_call(auto p1, auto p2, auto p3, auto p4,auto p5)
 {
 	return_value_type ret;
-	auto r10 __asm__("r10") = p4;
-	auto r8 __asm__("r8") = p5;
 	__asm__ __volatile__
 	(
+	"mov %5, %%r10;"
+	"mov %6, %%r8;"
 	"syscall"
 	: "=a" (ret)
 	//EDI      RSI       RDX
-	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"(r10), "r"(r8)
-	: "rcx", "r11", "memory", "cc"
+	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"((std::uint64_t)p4), "r"((std::uint64_t)p5)
+	: "rcx", "r11", "r10", "r8", "memory", "cc"
 	);
 	return ret;
 }
@@ -122,16 +121,16 @@ requires (1<sizeof(return_value_type))
 inline auto system_call(auto p1, auto p2, auto p3, auto p4,auto p5,auto p6)
 {
 	return_value_type ret;
-	auto r10 __asm__("r10") = p4;
-	auto r8 __asm__("r8") = p5;
-	auto r9 __asm__("r9") = p6;
 	__asm__ __volatile__
 	(
+	"mov %5, %%r10;"
+	"mov %6, %%r8;"
+	"mov %7, %%r9;"
 	"syscall"
 	: "=a" (ret)
 	//EDI      RSI       RDX
-	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"(r10),"r"(r8),"r"(r9)
-	: "rcx", "r11", "memory", "cc"
+	: "0"(syscall_number), "D"(p1), "S"(p2), "d"(p3), "r"((std::uint64_t)r10),"r"((std::uint64_t)r8),"r"((std::uint64_t)r9)
+	: "rcx", "r11", "r10", "r8", "r9", "memory", "cc"
 	);
 	return ret;
 }
