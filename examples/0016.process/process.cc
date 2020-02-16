c53c88e4123e64bfb1cb40666c803d667746734c
expnkx <euloanty@live.com>
2020-02-16 01:45:29 -0500

    posix

--- examples/0016.process/process.cc
+++ examples/0016.process/process.cc
@@ -3,9 +3,15 @@
 int main()
 try
 {
+	fast_io::native_pipe pipe;
 	fast_io::posix_waiting_process process;
 	if(is_child(process))
+	{
+		pipe.in().close();
 		fast_io::posix_exec("/usr/local/bin/gcc",{"-v"});
+	}
+	pipe.out().close();
+	transmit(fast_io::out,pipe);
 }
 catch(std::exception const& e)
 {
