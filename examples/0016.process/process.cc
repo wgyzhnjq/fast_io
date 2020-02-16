0d767ff5d78b2c8f47de50f70681337ded0ae951
expnkx <euloanty@live.com>
2020-02-16 17:43:29 -0500

    posix only

--- examples/0016.process/process.cc
+++ examples/0016.process/process.cc
@@ -4,21 +4,19 @@
 int main()
 try
 {
-//	fast_io::native_pipe pipe;
-	fast_io::jprocess process;
+	fast_io::native_pipe pipe;
+	fast_io::posix_jprocess process;
 	if(is_child(process))
 	{
-		print("I am child\n");
-//		pipe.in().close();
-//		fast_io::out=pipe.out();
-//		pipe.out().close();
-//		abort();
-//		fast_io::posix_exec("gcc",{"--version"});
+		pipe.in().close();
+		fast_io::out=pipe.out();
+		fast_io::err=pipe.out();
+		pipe.out().close();
+		fast_io::posix_exec("gcc",{"--version"});
 	}
-	print("I am parent\n");
-//	pipe.out().close();
-//	fast_io::onative_file onv("pipe.txt");
-//	transmit(onv,pipe);
+	pipe.out().close();
+	fast_io::onative_file onv("pipe.txt");
+	transmit(onv,pipe);
 }
 catch(std::exception const& e)
 {
