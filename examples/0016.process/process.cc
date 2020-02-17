11058a6ac1ee5cdaf56b5a4eb7aa5c6fb8b69a5c
expnkx <euloanty@live.com>
2020-02-17 03:52:43 -0500

    reset avoid double free on pipes

--- examples/0016.process/process.cc
+++ examples/0016.process/process.cc
@@ -5,11 +5,13 @@ int main()
 try
 {
 	fast_io::native_pipe pipe;
-	fast_io::jprocess process("/usr/local/bin/g++",{"--version"},{.out=pipe});
+	fast_io::jprocess process("/usr/local/bin/g++",{"-v"},{.out=pipe,.err=pipe});
 	fast_io::native_pipe pipe1;
-	fast_io::jprocess process1("./transmit",{},{.in=pipe,.out=pipe1});
+	fast_io::jprocess process1("./transmit",{},{.in=pipe,.out=pipe1,.err=pipe1});
+	fast_io::native_pipe pipe2;
+	fast_io::jprocess process2("./transmit",{},{.in=pipe1,.out=pipe2,.err=pipe2});
 	fast_io::onative_file onv("on.txt");
-	transmit(onv,pipe1);
+	transmit(onv,pipe2);
 }
 catch(std::exception const& e)
 {
