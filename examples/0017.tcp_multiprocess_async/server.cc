5c6a9b03bd5e63ebff2903010746d51560e66966
expnkx <unlvsur@live.com>
2020-02-17 13:54:38 -0500

    tcp multiprocess async server

---
+++ examples/0017.tcp_multiprocess_async/server.cc
@@ -0,0 +1,13 @@
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_device.h"
+#include"../../include/fast_io_network.h"
+
+int main(int argc,char** argv)
+{
+	fast_io::onative_file log("log.txt");
+	for(fast_io::tcp_server hd(2000);;)
+	{
+		fast_io::acceptor accept(hd);
+		fast_io::process proc("./server_child_process",{},{.in=accept,.out=accept,.err=log});
+	}
+}
---
+++ examples/0017.tcp_multiprocess_async/server_child_process.cc
@@ -0,0 +1,7 @@
+#include"../../include/fast_io.h"
+
+int main()
+{
+	print("Hello World from socket Child Process\n");
+	print(fast_io::err,"err\n");
+}
