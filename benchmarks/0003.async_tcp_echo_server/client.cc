e447df390655052ca9d59dead667cb846ad4fb56
expnkx <euloanty@live.com>
2020-02-17 22:41:21 -0500

    no stop

--- benchmarks/0003.async_tcp_echo_server/client.cc
+++ benchmarks/0003.async_tcp_echo_server/client.cc
@@ -1,13 +1,11 @@
 #include"../../include/fast_io_network.h"
 #include"../../include/fast_io_device.h"
-#include"../timer.h"
 
 int main()
 {
-	fast_io::timer tm("fast_io tcp multithread");
-	std::jthread jt;
+	//impossible to stop
 	fast_io::basic_obuf<fast_io::tcp_client> hd(fast_io::ipv4{127,0,0,1},2000);
-	jt=std::jthread([h=hd.native_handle()]() mutable
+	std::jthread jt([h=hd.native_handle()]() mutable
 	{
 		fast_io::obuf_file obf("client.txt");
 		transmit(obf,h);
