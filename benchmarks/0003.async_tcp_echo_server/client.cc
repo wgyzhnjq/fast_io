af00ed39c568115443504c4aa737363aeb50dbf8
expnkx <euloanty@live.com>
2020-02-17 21:35:34 -0500

    client

--- benchmarks/0003.async_tcp_echo_server/client.cc
+++ benchmarks/0003.async_tcp_echo_server/client.cc
@@ -5,8 +5,7 @@
 int main()
 {
 	fast_io::timer tm("fast_io tcp multithread");
-	fast_io::obuf_file obf("client.txt");
 	fast_io::tcp_client hd(fast_io::ipv4{127,0,0,1},2000);
 	print(hd,"Hello World\n");
-	transmit(obf,hd);
+	transmit(fast_io::out,hd);
 }
