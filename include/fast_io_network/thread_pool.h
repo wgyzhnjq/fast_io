251fcb5ecc038a7ef13f5cceec083f7df576b821
expnkx <euloanty@live.com>
2020-02-18 03:21:26 -0500

    server

--- include/fast_io_network/thread_pool.h
+++ include/fast_io_network/thread_pool.h
@@ -8,20 +8,20 @@ https://www.youtube.com/watch?v=c1gO9aB9nbs&t=3166s
 CppCon 2014: Herb Sutter "Lock-Free Programming (or, Juggling Razor Blades), Part I"
 */
 template<std::movable acceptor_type,std::size_t size=900,typename server_type,typename Func>
-inline void thread_pool_accept_callback(server_type& server,Func&& func)
+inline void thread_pool_accept(server_type& server,Func&& func)
 {
 	std::vector<std::thread> pool;
 	pool.reserve(size);
-	std::mutex mtx;
-	std::condition_variable cv;
+	std::array<std::pair<std::mutex,std::condition_variable>,size> cvs;
 	std::vector<std::optional<acceptor_type>> slot(size);
 	for(std::size_t i{};i!=size;++i)
-		pool.emplace_back([func,&mtx,&cv,&slot](std::size_t i)
+		pool.emplace_back([func,&cvs,&slot](std::size_t i)
 		{
+			auto &cvp(cvs[i]);
 			for(std::optional<acceptor_type>& opt(slot[i]);;)
 			{
-				std::unique_lock ul{mtx};
-				cv.wait(ul,[&opt](){return opt.has_value();});
+				std::unique_lock ul{cvp.first};
+				cvp.second.wait(ul,[&opt](){return opt.has_value();});
 				auto acc(*std::move(opt));
 				opt.reset();
 				func(acc);
@@ -34,7 +34,7 @@ inline void thread_pool_accept_callback(server_type& server,Func&& func)
 			if(++i==e)
 				i=slot.begin();
 		*i=std::move(opt);
-		cv.notify_all();
+		cvs[i-slot.begin()].second.notify_one();
 	}
 }
 
