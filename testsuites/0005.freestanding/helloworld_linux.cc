b5778023a827c42ab19bb0a66c3e894c633e71c8
expnkx <euloanty@live.com>
2020-02-22 16:16:24 -0500

    fast exit

--- testsuites/0005.freestanding/helloworld_linux.cc
+++ testsuites/0005.freestanding/helloworld_linux.cc
@@ -3,7 +3,7 @@
 extern "C" void _start() noexcept
 {
 	print("Hello World\n");
-	fast_io::fast_terminate(0);
+	fast_io::fast_exit(0);
 }
 
 //g++ -o helloworld_linux helloworld_linux.cc -Ofast -std=c++2a -s -flto -fno-exceptions -fno-rtti -DNDEBUG
