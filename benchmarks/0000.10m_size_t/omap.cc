5d9dcafb7ba7cdb5d731fa935cc89a9505253a61
expnkx <euloanty@live.com>
2020-02-29 01:43:19 -0500

    omap

--- benchmarks/0000.10m_size_t/omap.cc
+++ benchmarks/0000.10m_size_t/omap.cc
@@ -5,5 +5,8 @@
 int main()
 {
 	constexpr std::size_t N=10000000;
-
+	fast_io::timer tm("omap");
+	fast_io::omap_file pf("om.txt",	fast_io::open_interface<fast_io::open_mode::trunc|fast_io::open_mode::in|fast_io::open_mode::out|fast_io::open_mode::binary>);
+	for(std::size_t i{};i!=N;++i)
+		println(pf,i);
 }
