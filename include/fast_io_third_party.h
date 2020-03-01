ee9e28a872d9f27511a3dee9c7ba6f237191b8b3
expnkx <euloanty@live.com>
2020-03-01 00:55:54 -0500

    mfc

--- include/fast_io_third_party.h
+++ include/fast_io_third_party.h
@@ -2,6 +2,7 @@
 #include"fast_io_hosted.h"
 //fast_io_third_party is to support 3rd-party non standard I/O libraries like MFC, QT, cppwinrt
 #ifdef _AFXDLL
+//MFC/ATL support
 #include<afx.h>
 #include"fast_io_third_party/mfc.h"
 #endif
