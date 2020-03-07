6a8917341394f3b0905566210c2a1eec9e6a4f40
zyddnys <zyddnys@outlook.com>
2020-03-06 22:20:32 -0500

    finished rc6

--- testsuites/0002.crypto/cbc.cc
+++ testsuites/0002.crypto/cbc.cc
@@ -7,13 +7,13 @@ int main()
     std::array<std::uint8_t, 16> const key{1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
     std::array<std::uint8_t, 16> const iv{1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
     {
-        fast_io::crypto::ocbc_encrypt<fast_io::obuf_file, fast_io::crypto::speck::speck_enc_128_128> 
+        fast_io::crypto::ocbc_encrypt<fast_io::obuf_file, fast_io::crypto::rc6::rc6_enc_128> 
             ob(std::piecewise_construct,std::forward_as_tuple("text.txt"),
             std::forward_as_tuple(std::as_bytes(std::span(key)), std::as_bytes(std::span(iv))));
         print(ob,"hello world\nxxxx\n8988\n");
     }
     {
-        fast_io::crypto::icbc_decrypt<fast_io::ibuf_file, fast_io::crypto::speck::speck_dec_128_128> 
+        fast_io::crypto::icbc_decrypt<fast_io::ibuf_file, fast_io::crypto::rc6::rc6_dec_128> 
             ib(std::piecewise_construct,std::forward_as_tuple("text.txt"),
             std::forward_as_tuple(std::as_bytes(std::span(key)), std::as_bytes(std::span(iv))));
         transmit(fast_io::out,ib);
