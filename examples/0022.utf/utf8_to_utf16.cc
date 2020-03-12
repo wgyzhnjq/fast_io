45f0ac2f1174c1e81ef893636b197b0608e549cf
euloanty <euloanty@live.com>
2020-03-12 22:18:20 +0000

    tf

--- examples/0022.utf/utf8_to_utf16.cc
+++ examples/0022.utf/utf8_to_utf16.cc
@@ -5,7 +5,7 @@
 int main()
 {
 	using namespace std::string_view_literals;
-	auto wstr{fast_io::concat<std::u16string>(fast_io::code_cvt(u8"abcdefgh"sv))};
+	auto wstr{fast_io::concat<std::u16string>(fast_io::code_cvt(u8"lol呵呵. 开心就好。234713872947194748231asjfosajfsioafj"sv))};
 	for(auto const & e : wstr)
 		println(fast_io::c_stdout,e);
 }
