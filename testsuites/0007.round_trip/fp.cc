91f3393b0f15b589a61f67d6653213e3cde7c0c8
expnkx <euloanty@live.com>
2020-03-19 21:23:32 -0400

    fp

---
+++ testsuites/0007.round_trip/fp.cc
@@ -0,0 +1,17 @@
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_legacy.h"
+#ifdef _MSC_VER
+#include<charconv>
+#endif
+
+int main()
+{
+	double d(23535782139572189357.0);
+	print(fast_io::c_stdout,u8"fast_io:",d,u8"\nmicrosoft STL charconv:");
+	char buffer[100];
+	auto [p,ec]=std::to_chars(buffer,buffer+100,d);
+	*p=u8'\n';
+	write(fast_io::out,buffer,++p);
+	put(fast_io::c_stdout,u8'\n');
+}
+//cl -EHsc -Ox fp.cc /std:c++latest /experimental:concepts
