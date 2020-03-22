dae3677cebb31e3662527af6a90757e5e169d58f
expnkx <euloanty@live.com>
2020-03-22 04:45:22 -0400

    glibc hack

--- include/fast_io_legacy_impl/c/glibc.h
+++ include/fast_io_legacy_impl/c/glibc.h
@@ -3,8 +3,52 @@
 namespace fast_io
 {
 
+inline constexpr char* ibuffer_cbegin(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_read_base;
+}
 
+inline constexpr auto& ibuffer_curr(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_read_ptr;
+}
 
+inline constexpr char* ibuffer_cend(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_read_end;
+}
+extern "C" int __underflow (FILE *);
+inline bool underflow(c_io_observer_unlocked cio)
+{
+	return __underflow(cio.fp)!=EOF;
+}
+
+inline constexpr char* obuffer_cbegin(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_write_base;
+}
+
+inline constexpr auto& obuffer_curr(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_write_ptr;
+}
+
+inline constexpr char* obuffer_cend(c_io_observer_unlocked cio)
+{
+	return cio.fp->_IO_write_end;
+}
+extern "C" int __overflow (FILE *,int);
+
+inline void overflow(c_io_observer_unlocked cio,char ch)
+{
+	if(__overflow(cio.fp,static_cast<int>(static_cast<unsigned char>(ch)))==EOF)[[unlikely]]
+#ifdef __cpp_exceptions
+		throw std::system_error(errno,std::generic_category());
+#else
+		fast_terminate();
+#endif
+}
+/*
 #if defined(__GNU_LIBRARY__)
 inline char* oreserve(c_io_observer_unlocked& cfhd,std::size_t n)
 {
@@ -18,5 +62,5 @@ inline void orelease(c_io_observer_unlocked& cfhd,std::size_t n)
 {
 	cfhd.native_handle()->_IO_write_ptr-=n;
 }
-#endif
+#endif*/
 }
