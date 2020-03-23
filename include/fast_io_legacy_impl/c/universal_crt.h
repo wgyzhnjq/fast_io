bd52be963d0ae4835e01f1f0197c6461ac389c9a
expnkx <euloanty@live.com>
2020-03-23 09:29:18 -0400

    universal crt

--- include/fast_io_legacy_impl/c/universal_crt.h
+++ include/fast_io_legacy_impl/c/universal_crt.h
@@ -12,6 +12,10 @@ C:\Program Files (x86)\Windows Kits\10\Source\10.0.19041.0\ucrt
 // Internal Stream Types (__crt_stdio_stream and friends)
 //
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+
+// Ensure that __crt_stdio_stream_data* and FILE* pointers are freely convertible:
+*/
 struct __crt_stdio_stream_data
 {
     union
@@ -27,12 +31,7 @@ struct __crt_stdio_stream_data
     int              _charbuf;
     int              _bufsiz;
     char*            _tmpfname;
-    CRITICAL_SECTION _lock;
 };
-
-// Ensure that __crt_stdio_stream_data* and FILE* pointers are freely convertible:
-*/
-
 
 inline char* ibuffer_begin(c_io_observer_unlocked cio)
 {
@@ -49,15 +48,16 @@ inline char* ibuffer_end(c_io_observer_unlocked cio)
 	return reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr+reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_cnt;
 }
 
-inline constexpr void ibuffer_set_curr(c_io_observer_unlocked cio,char* ptr)
+inline void ibuffer_set_curr(c_io_observer_unlocked cio,char* ptr)
 {
 	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_cnt-=ptr-reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr;
 	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr=ptr;
 }
+extern "C" int __stdcall __acrt_stdio_refill_and_read_narrow_nolock(FILE*) noexcept;
 
 inline bool underflow(c_io_observer_unlocked cio)
 {
-	if(_filbuf(reinterpret_cast<__crt_stdio_stream_data*>(cio.fp))==EOF)[[unlikely]]
+	if(__acrt_stdio_refill_and_read_narrow_nolock(cio.fp)==EOF)[[unlikely]]
 		return false;
 	++reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_cnt;
 	--reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr;
@@ -84,11 +84,11 @@ inline void obuffer_set_curr(c_io_observer_unlocked cio,char* ptr)
 	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_cnt-=ptr-reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr;
 	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_ptr=ptr;
 }
-
+extern "C" int __stdcall __acrt_stdio_flush_and_write_narrow_nolock(int,FILE*) noexcept;
 inline void overflow(c_io_observer_unlocked cio,char ch)
 {
-	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_flag|=0x010000;
-	if(_flsbuf(static_cast<int>(static_cast<unsigned char>(ch)),reinterpret_cast<__crt_stdio_stream_data*>(cio.fp))==EOF)[[unlikely]]
+	reinterpret_cast<__crt_stdio_stream_data*>(cio.fp)->_flags|=0x010000;
+	if(__acrt_stdio_flush_and_write_narrow_nolock(static_cast<int>(static_cast<unsigned char>(ch)),cio.fp)==EOF)[[unlikely]]
 #ifdef __cpp_exceptions
 		throw std::system_error(errno,std::generic_category());
 #else
