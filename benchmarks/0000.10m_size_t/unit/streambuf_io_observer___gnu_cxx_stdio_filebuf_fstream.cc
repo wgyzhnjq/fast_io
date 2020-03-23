b409c10980d42b652d49f86a621283af2fd101c2
expnkx <euloanty@live.com>
2020-03-23 05:55:43 -0400

    fstream

---
+++ benchmarks/0000.10m_size_t/unit/streambuf_io_observer___gnu_cxx_stdio_filebuf_fstream.cc
@@ -0,0 +1,33 @@
+#include"../../timer.h"
+#include<fstream>
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+//#include"../../include/fast_io_crypto.h"
+#include"../../../include/fast_io_legacy.h"
+#include<ext/stdio_filebuf.h>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	{
+	fast_io::timer t("output");
+	fast_io::posix_file posix("streambuf_io_observer___gnu_cxx_stdio_filebuf.txt",fast_io::open_interface<fast_io::open_mode::binary|fast_io::open_mode::out>);
+	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::out,131072);
+	std::ostream ostm(&stdio_filebuf);
+//	fast_io::streambuf_io_observer fob{std::addressof(stdio_filebuf)};
+	for(std::size_t i{};i!=N;++i)
+	{
+		ostm<<i;
+		stdio_filebuf.sputc('\n');
+	}
+	}
+	std::vector<std::size_t> vec(N);
+	{
+	fast_io::timer t("input");
+	fast_io::posix_file posix("streambuf_io_observer___gnu_cxx_stdio_filebuf.txt",fast_io::open_interface<fast_io::open_mode::binary|fast_io::open_mode::in>);
+	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::in,65536);
+	fast_io::streambuf_io_observer fob{std::addressof(stdio_filebuf)};
+	for(std::size_t i{};i!=N;++i)
+		scan(fob,vec[i]);
+	}
+}
