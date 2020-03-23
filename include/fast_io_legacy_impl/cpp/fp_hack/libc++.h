ea4841057e775279457452ecc0c3494f368e8b24
expnkx <euloanty@live.com>
2020-03-23 11:13:20 -0400

    libc++

---
+++ include/fast_io_legacy_impl/cpp/fp_hack/libc++.h
@@ -0,0 +1,9 @@
+#pragma once
+//https://github.com/llvm-mirror/libcxx/blob/master/include/fstream
+
+//Todo. Too hard + no platform + no document
+
+namespace fast_io::details::streambuf_hack
+{
+
+}
