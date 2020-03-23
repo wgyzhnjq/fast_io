38ee502be4ae0685913c9b81e9642025e4659be6
expnkx <euloanty@live.com>
2020-03-23 05:58:06 -0400

    1048576

--- benchmarks/0000.10m_size_t/unit/streambuf_io_observer___gnu_cxx_stdio_filebuf.cc
+++ benchmarks/0000.10m_size_t/unit/streambuf_io_observer___gnu_cxx_stdio_filebuf.cc
@@ -12,7 +12,7 @@ int main()
 	{
 	fast_io::timer t("output");
 	fast_io::posix_file posix("streambuf_io_observer___gnu_cxx_stdio_filebuf.txt",fast_io::open_interface<fast_io::open_mode::binary|fast_io::open_mode::out>);
-	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::out,65536);
+	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::out,1048576);
 	fast_io::streambuf_io_observer fob{std::addressof(stdio_filebuf)};
 	for(std::size_t i{};i!=N;++i)
 		println(fob,i);
@@ -21,7 +21,7 @@ int main()
 	{
 	fast_io::timer t("input");
 	fast_io::posix_file posix("streambuf_io_observer___gnu_cxx_stdio_filebuf.txt",fast_io::open_interface<fast_io::open_mode::binary|fast_io::open_mode::in>);
-	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::in,65536);
+	__gnu_cxx::stdio_filebuf<char> stdio_filebuf(posix.native_handle(),std::ios::binary|std::ios::in,1048576);
 	fast_io::streambuf_io_observer fob{std::addressof(stdio_filebuf)};
 	for(std::size_t i{};i!=N;++i)
 		scan(fob,vec[i]);
