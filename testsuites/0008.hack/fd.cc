9ab7622f8ef2b5c5fc5c752a645150a2bcba299b
expnkx <euloanty@live.com>
2020-03-23 11:16:16 -0400

    fd

--- testsuites/0008.hack/fd.cc
+++ testsuites/0008.hack/fd.cc
@@ -7,7 +7,8 @@ int main()
 	fast_io::streambuf_io_observer siob{std::cout.rdbuf()};
 	auto cio{static_cast<fast_io::c_io_observer>(siob)};
 	auto pio{static_cast<fast_io::posix_io_observer>(siob)};
-	println(u8"fp:",fast_io::unsigned_view(cio.native_handle()),u8"\nfd:",pio.native_handle()
+	println(u8"std::cout.rdbuf():",fast_io::unsigned_view(siob.native_handle()),
+	u8"\nFILE*:",fast_io::unsigned_view(cio.native_handle()),u8"\nfile descriptor:",pio.native_handle()
 #if defined(__WINNT__) || defined(_MSC_VER)
 	,u8"\nwin32 handle:",fast_io::unsigned_view(static_cast<fast_io::win32_io_observer>(siob).native_handle())
 	,u8"\nnt handle:",fast_io::unsigned_view(static_cast<fast_io::win32_io_observer>(siob).native_handle())
