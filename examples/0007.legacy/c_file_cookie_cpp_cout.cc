2341d796102d0c2b6bf823d6fdada05e84e08114
expnkx <euloanty@live.com>
2020-03-25 22:38:16 -0400

    cpp_cout

--- examples/0007.legacy/c_file_cookie_cpp_cout.cc
+++ examples/0007.legacy/c_file_cookie_cpp_cout.cc
@@ -9,7 +9,8 @@ int main()
 	fast_io::streambuf_io_observer smv{std::cout.rdbuf()};
 	fast_io::c_file cf(fast_io::c_file_cookie,"wb",std::ref(smv));
 	fprintf(cf.native_handle(),"Hello World from fprintf, number: %d\n",5);
-	println(cf,"Hello World from fast_io::println, number: ",100);
+	println(cf,"Hello World from fast_io::println for c_file, number: ",100);
 	flush(cf);
 	std::cout<<"Hello World from std::cout\n";
+	println(smv,"Hello World from fast_io::println from io_observer, number: ",16);
 }
