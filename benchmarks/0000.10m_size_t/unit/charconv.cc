d4cea32938b2cd4fc71a68bd0945e3a2a32b02cd
expnkx <euloanty@live.com>
2020-03-26 21:58:57 -0400

    char

--- benchmarks/0000.10m_size_t/unit/charconv.cc
+++ benchmarks/0000.10m_size_t/unit/charconv.cc
@@ -1,15 +1,19 @@
 #include<charconv>
-#include"../../include/fast_io_device.h"
+#include"../../timer.h"
+#include"../../../include/fast_io_device.h"
 
 int main()
 {
 	constexpr std::size_t n{10000000};
-	fast_io::obuf_file obf("charconv.txt");
-	std::array<char,50> buffer;
-	for(std::size_t i{};i!=n;++i)
 	{
-		auto p=std::to_chars(buffer.data(),buffer.data()+buffer.size(),i);
-		*p=u8'\n';
-		write(obf,buffer.data(),++p);
+		fast_io::timer t("output");
+		fast_io::obuf_file obf("charconv.txt");
+		std::array<char,50> buffer;
+		for(std::size_t i{};i!=n;++i)
+		{
+			auto [p,ec]=std::to_chars(buffer.data(),buffer.data()+buffer.size(),i);
+			*p=u8'\n';
+			write(obf,buffer.data(),++p);
+		}
 	}
 }
