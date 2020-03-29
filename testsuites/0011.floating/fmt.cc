dd1c7799db08c4f459001f0d45bf262959e9a08e
expnkx <euloanty@live.com>
2020-03-29 02:58:16 -0400

    prnt

--- testsuites/0011.floating/fmt.cc
+++ testsuites/0011.floating/fmt.cc
@@ -1,10 +1,10 @@
 #include<fmt/format.h>
-#include"../../include/fast_io.h"
+//#include"../../include/fast_io.h"
 
 int main()
 {
 	if(10==fast_io::to<int>("10"))
-		print("Hello I am 10\n");
+		fmt::print("Hello I am 10\n");
 	else
-		print("no I am not 10\n");	
+		fmt::print("no I am not 10\n");	
 }
