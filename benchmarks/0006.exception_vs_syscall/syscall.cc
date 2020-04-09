90f7bb8f44f9da736e70c522a838c399c0af108e
expnkx <euloanty@live.com>
2020-04-09 01:58:01 -0400

    syscall

--- benchmarks/0006.exception_vs_syscall/syscall.cc
+++ benchmarks/0006.exception_vs_syscall/syscall.cc
@@ -3,9 +3,9 @@
 
 int main()
 {
-	fast_io::timer tm("::write");
+	fast_io::timer tm("::close");
 	for(std::size_t i{};i!=1000000;++i)
 	{
-		::write(10,nullptr,0);  //write to an invalid fd
+		close(1);
 	}
 }
