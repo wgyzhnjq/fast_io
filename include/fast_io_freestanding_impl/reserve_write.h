f5ea8a36066ec50e0d605353f633d773285b75a3
expnkx <euloanty@live.com>
2020-04-13 18:41:11 -0400

    reserve_write

--- include/fast_io_freestanding_impl/reserve_write.h
+++ include/fast_io_freestanding_impl/reserve_write.h
@@ -6,23 +6,23 @@ namespace fast_io
 
 namespace details
 {
-template<output_stream output,Func func>
+template<output_stream output,typename Func>
 constexpr void reserve_write_cold_path(output& out,std::size_t required_size,Func& func)
 {
 	if(required_size<=1024)[[likely]]
 	{
 		std::array<typename output::char_type,1024> temp_buffer;
-		write_all(out,temp_buffer.data(),func(iter.data()));
+		write_all(out,temp_buffer.data(),func(temp_buffer.data()));
 	}
 	else
 	{
-		temp::temp_unique_arr_ptr<typename output::char_type> temp_buffer(required_size);
-		write_all(out,temp_buffer.data(),func(iter.data()));
+		details::temp_unique_arr_ptr<typename output::char_type> temp_buffer(required_size);
+		write_all(out,temp_buffer.data(),func(temp_buffer.data()));
 	}
 }
 }
 
-template<output_stream output,Func func>
+template<output_stream output,typename Func>
 inline constexpr void reserve_write(output& out,std::size_t required_size,Func func)
 {
 	if constexpr(reserve_output_stream<output>)
