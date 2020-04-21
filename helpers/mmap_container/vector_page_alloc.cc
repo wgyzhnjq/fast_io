7653f85c0ba4445c98bc2c91e0029dae0607e661
expnkx <euloanty@live.com>
2020-04-21 02:55:46 -0400

    vec

--- helpers/mmap_container/vector_page_alloc.cc
+++ helpers/mmap_container/vector_page_alloc.cc
@@ -1,20 +1,23 @@
 #include"../timer.h"
+#include"../../include/fast_io_device.h"
 #include<vector>
 #include"page_alloc.h"
 
 int main()
 {
-	constexpr std::size_t N{100};
-	constexpr std::size_t M{1000000};
+	constexpr std::size_t N{1000000};
+	constexpr std::size_t M{100};
 
-	std::size_t sum{};
+	fast_io::obuf_file obf("vec.txt");
 	fast_io::timer tm("std::vector");
 
 	for(std::size_t i{};i!=N;++i)
 	{
 		std::vector<std::size_t> vec;
+//		vec.reserve(M);
 		for(std::size_t j{};j!=M;++j)
 			vec.emplace_back(j);
+
+		println(obf,fast_io::unsigned_view(vec.data()));
 	}
-	println("sum:",sum);
 }
