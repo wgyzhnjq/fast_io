9633dadd3d35cb36ef9d00b9a3e4174186ceabc7
expnkx <euloanty@live.com>
2020-04-21 01:00:40 -0400

    vec

--- helpers/mmap_container/vector.cc
+++ helpers/mmap_container/vector.cc
@@ -1,14 +1,21 @@
 #include"../timer.h"
 #include<vector>
 
+struct sf
+{
+	std::size_t v;
+	constexpr sf();
+};
+
 int main()
 {
-	constexpr std::size_t N{1000};
+	constexpr std::size_t N{1};
 	constexpr std::size_t M{1000000};
 
-	std::vector<std::size_t> vec(M);
 	std::size_t sum{};
 	fast_io::timer tm("std::vector");
+	std::vector<std::size_t> vec(M);
+
 	for(std::size_t i{};i!=N;++i)
 	{
 	for(auto & e : vec)
--- helpers/mmap_container/vector_page_alloc.cc
+++ helpers/mmap_container/vector_page_alloc.cc
@@ -2,15 +2,21 @@
 #include<vector>
 #include"page_alloc.h"
 
+struct sf
+{
+	std::size_t v;
+	constexpr sf();
+};
+
 int main()
 {
-	constexpr std::size_t N{1000};
+	constexpr std::size_t N{1};
 	constexpr std::size_t M{1000000};
 
 	std::size_t sum{};
-
-	std::vector<std::size_t> vec(M);
 	fast_io::timer tm("std::vector");
+	std::vector<std::size_t> vec(M);
+
 	for(std::size_t i{};i!=N;++i)
 	{
 	for(auto & e : vec)
