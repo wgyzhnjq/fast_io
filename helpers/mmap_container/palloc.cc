8fe7dc7998a1d2c835556f4b53e2064004ddb539
expnkx <euloanty@live.com>
2020-04-21 04:14:03 -0400

    pa

--- helpers/mmap_container/palloc.cc
+++ helpers/mmap_container/palloc.cc
@@ -1,9 +1,17 @@
-
-#include"../../include/fast_io.h"
-#include"page_alloc.h"
-#include<vector>
+#include"../../include/fast_io_device.h"
+#include"../../include/fast_io_in.h"
+#include"../timer.h"
 
 int main()
 {
-	std::unique_ptr<std::size_t> uptr{new std::size_t};
+	std::size_t n,m;
+	scan(n,m);
+	fast_io::obuf_file obf("log.txt");
+	fast_io::timer tm("timer");
+	for(std::size_t i{};i!=n;++i)
+	{
+		auto ptr{new std::size_t[m]};
+		println(obf,fast_io::unsigned_view(ptr));
+		delete[] ptr;
+	}
 }
