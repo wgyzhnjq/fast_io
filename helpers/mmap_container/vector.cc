0353fc63a8148052eadcec72903264c32b844035
expnkx <euloanty@live.com>
2020-04-21 04:44:21 -0400

    vect

--- helpers/mmap_container/vector.cc
+++ helpers/mmap_container/vector.cc
@@ -6,16 +6,17 @@ int main()
 	constexpr std::size_t N{100};
 	constexpr std::size_t M{1000000};
 
-	std::vector<std::size_t> vec(M,1);
+	
 	std::size_t sum{};
 	fast_io::timer tm("std::vector");
 	for(std::size_t i{};i!=N;++i)
 	{
-	for(auto & e : vec)
-	{
-		sum+=e;
-		e+=sum;
-	}
+		std::vector<std::size_t> vec(M,1);
+		for(auto & e : vec)
+		{
+			sum+=e;
+			e+=sum;
+		}
 	}
 	println("sum:",sum);
 }
