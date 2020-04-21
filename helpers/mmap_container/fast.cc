e69406ebfe5ae4d7e05e708d505ebc117f5efdf2
expnkx <euloanty@live.com>
2020-04-21 05:03:23 -0400

    sv

--- helpers/mmap_container/fast.cc
+++ helpers/mmap_container/fast.cc
@@ -1,20 +1,13 @@
-#include"../timer.h"
-#include<vector>
-#include"page_alloc.h"
+#include"../../include/fast_io_device.h"
+#include"../../include/fast_io_in.h"
 
 int main()
 {
 	constexpr std::size_t N{100000};
 	constexpr std::size_t M{100};
-
-	std::size_t sum{};
-	fast_io::timer tm("std::vector");
-
-	for(std::size_t i{};i!=N;++i)
+	for(std::size_t i{};i!=2;++i)
 	{
-		std::size_t* bt{new std::size_t[M]};
-		bt[M-1]={};
-		sum += bt[M>>1];
-		delete[] bt;
+	auto ptr{new int};
+	delete ptr;
 	}
 }
