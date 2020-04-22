2fff84001b0fbbebef6578b7a121ac5e3f39b24a
expnkx <euloanty@live.com>
2020-04-22 07:37:55 -0400

    general

---
+++ benchmarks/0001.10m_double/general/obuf.cc
@@ -0,0 +1,24 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+//#include"../../include/fast_io_crypto.h"
+#include"../../../include/fast_io_legacy.h"
+#include<random>
+
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	std::vector<double> vec;
+	vec.reserve(N);
+	std::mt19937_64 eng;
+	std::uniform_real_distribution dis(DBL_MIN,DBL_MAX);
+	for(std::size_t i(0);i!=N;++i)
+		vec.emplace_back(dis(eng));
+	{
+	fast_io::timer t("obuf file normal output");
+	fast_io::obuf_file obf("obuf_file.txt");
+	for(std::size_t i{};i!=N;++i)
+		println(obf,vec[i]);
+	}
+}
