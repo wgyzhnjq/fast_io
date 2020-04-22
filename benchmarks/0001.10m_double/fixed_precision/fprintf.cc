fa6e49b176f44558171a1858790a14a938c67468
expnkx <euloanty@live.com>
2020-04-22 07:28:41 -0400

    allocation cost

---
+++ benchmarks/0001.10m_double/fixed_precision/fprintf.cc
@@ -0,0 +1,23 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+//#include"../../include/fast_io_crypto.h"
+#include"../../../include/fast_io_legacy.h"
+#include<random>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	std::vector<double> vec;
+	vec.reserve(N);
+	std::mt19937_64 eng;
+	std::uniform_real_distribution dis(DBL_MIN,DBL_MAX);
+	for(std::size_t i(0);i!=N;++i)
+		vec.emplace_back(dis(eng));
+	{
+	fast_io::timer t("output");
+	fast_io::c_file cf_out("fprintf.txt",fast_io::open_interface<fast_io::open_mode::out|fast_io::open_mode::binary>);
+	for(std::size_t i{};i!=N;++i)
+		fprintf(cf_out.native_handle(),"%.13f\n",vec[i]);
+	}
+}
