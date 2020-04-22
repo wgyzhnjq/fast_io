85669c5911cf93ff2c7fa73739038e9c1bae24e0
expnkx <euloanty@live.com>
2020-04-22 07:20:27 -0400

    precision

---
+++ benchmarks/0001.10m_double/scientific_precision/fstream.cc
@@ -0,0 +1,30 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+//#include"../../include/fast_io_crypto.h"
+#include"../../../include/fast_io_legacy.h"
+#include<random>
+#include<fstream>
+#include<iomanip>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	std::vector<double> vec;
+	vec.reserve(N);
+	std::mt19937_64 eng;
+	std::uniform_real_distribution dis(DBL_MIN,DBL_MAX);
+	for(std::size_t i(0);i!=N;++i)
+		vec.emplace_back(dis(eng));
+	{
+	fast_io::timer t("output");
+	std::ofstream fout("ofstream.txt",std::ofstream::binary);
+	auto& rdbuf(*fout.rdbuf());
+	fout<<std::setprecision(13)<<std::scientific;
+	for(std::size_t i{};i!=N;++i)
+	{
+		fout<<vec[i];
+		rdbuf.sputc('\n');
+	}
+	}
+}
---
+++ benchmarks/0001.10m_double/scientific_precision/parallel.cc
@@ -0,0 +1,26 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+//#include"../../include/fast_io_crypto.h"
+#include"../../../include/fast_io_legacy.h"
+#include<random>
+#include<thread>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	std::vector<double> vec;
+	vec.reserve(N);
+	std::mt19937_64 eng;
+	std::uniform_real_distribution dis(DBL_MIN,DBL_MAX);
+	for(std::size_t i(0);i!=N;++i)
+		vec.emplace_back(dis(eng));
+	{
+	fast_io::timer t("output");
+	fast_io::obuf_file obf("parallel.txt");
+	print(obf,fast_io::parallel(vec,[](auto const& e)
+	{
+		return fast_io::line(fast_io::scientific(e,13));
+	}));
+	}
+}
