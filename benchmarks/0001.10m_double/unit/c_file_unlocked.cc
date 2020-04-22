5844629a29cd13a8a56d13be824f216c3ea4443b
expnkx <euloanty@live.com>
2020-04-22 06:56:44 -0400

    no longer use random_device

--- benchmarks/0001.10m_double/unit/c_file_unlocked.cc
+++ benchmarks/0001.10m_double/unit/c_file_unlocked.cc
@@ -10,8 +10,7 @@ int main()
 	constexpr std::size_t N(10000000);
 	std::vector<double> vec;
 	vec.reserve(N);
-	std::random_device device;
-	std::mt19937_64 eng(device());
+	std::mt19937_64 eng;
 	std::uniform_real_distribution dis(-1000.0,1000.0);
 	for(std::size_t i(0);i!=N;++i)
 		vec.emplace_back(dis(eng));
