e3fc3f8f74814b509dce07649366691380af2401
expnkx <euloanty@live.com>
2020-04-30 12:50:19 -0400

    timer

--- benchmarks/timer.h
+++ benchmarks/timer.h
@@ -15,7 +15,9 @@ class timer
 	std::chrono::high_resolution_clock::time_point t0;
 public:
 	template<typename ...Args>
+#ifndef __clang__
 	requires std::constructible_from<std::string_view,Args...>
+#endif
 	timer(Args&& ...args):s(std::forward<Args>(args)...),t0(std::chrono::high_resolution_clock::now()){}
 	timer(const timer&) = delete;
 	timer& operator=(const timer&) = delete;
