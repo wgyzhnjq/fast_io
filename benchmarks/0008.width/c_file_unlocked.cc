461dad533ffa2696f2eca0c55fbae900013871b2
expnkx <euloanty@live.com>
2020-04-30 10:18:47 -0400

    width

--- benchmarks/0008.width/c_file_unlocked.cc
+++ benchmarks/0008.width/c_file_unlocked.cc
@@ -5,8 +5,8 @@
 #include"../timer.h"
 int main()
 {
-	fast_io::timer tm("c_file");
-	fast_io::c_file cf("c_file.txt",fast_io::open_interface<fast_io::open_mode::out|fast_io::open_mode::binary>);
+	fast_io::timer tm("c_file_unlocked");
+	fast_io::c_file_unlocked cf("c_file_unlocked.txt",fast_io::open_interface<fast_io::open_mode::out|fast_io::open_mode::binary>);
 	for(std::size_t i{};i!=10000000;++i)
 		println(cf,fast_io::right_width(i,20,u8'0'));
 }
