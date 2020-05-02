a6c8bb71f47c62458a0724516d524b13d844fc67
expnkx <euloanty@live.com>
2020-05-01 22:03:20 -0400

    openssl context

--- examples/0028.openssl/https/https_client.cc
+++ examples/0028.openssl/https/https_client.cc
@@ -1,7 +1,8 @@
 #include"../../../include/fast_io.h"
-#include"../../../include/fast_io_driver/openssl.h"
+#include"../../../include/fast_io_driver/openssl_driver.h"
 
 int main()
 {
-	
+	fast_io::openssl::ssl_context ssl_ctx(fast_io::openssl::tls_method::tls);
+	fast_io::openssl::ssl_file file(ssl_ctx);
 }
