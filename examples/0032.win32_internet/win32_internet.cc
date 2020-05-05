9ad4f8642cfc66fc6598b39a8bee74dc777efc96
expnkx <euloanty@live.com>
2020-05-05 19:45:04 -0400

    internet

---
+++ examples/0032.win32_internet/win32_internet.cc
@@ -0,0 +1,8 @@
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_driver/win32_internet.h"
+
+int main()
+{
+	fast_io::win32_internet_https_client https_client("www.warcraftlogs.com","GET","/zone/rankings/24/");
+	transmit(fast_io::out,https_client);
+}
