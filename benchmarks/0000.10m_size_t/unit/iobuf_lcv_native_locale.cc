8181fa3b37d7e40d28ab1d1ebb5517e268720294
expnkx <euloanty@live.com>
2020-05-17 04:47:39 -0400

    locale lcv

---
+++ benchmarks/0000.10m_size_t/unit/iobuf_lcv_native_locale.cc
@@ -0,0 +1,25 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+#include"../../../include/fast_io_legacy.h"
+#include"../../../include/fast_io_locale.h"
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	{
+	fast_io::timer t("output");
+	fast_io::c_locale loc(fast_io::c_locale_category::all);
+	fast_io::c_lconv_storage stg(loc);
+	fast_io::obuf_file obf("obuf_file_lcv_native.txt");
+	for(std::size_t i{};i!=N;++i)
+		println(obf,fast_io::lcv(stg,i));
+	}
+	std::vector<std::size_t> vec(N);
+	{
+	fast_io::timer t("input");
+	fast_io::ibuf_file ibf("obuf_file_lcv_native.txt");
+	for(std::size_t i{};i!=N;++i)
+		scan(ibf,vec[i]);
+	}
+}
