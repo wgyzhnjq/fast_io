aa370984d062e3fcee4f098f68a2ede802231247
expnkx <euloanty@live.com>
2020-05-16 20:24:35 -0400

    locale

---
+++ include/fast_io_locale.h
@@ -0,0 +1,5 @@
+#pragma once
+#include"fast_io_freestanding.h"
+#include<locale.h>
+#include"fast_io_locale/c_locale.h"
+#include"fast_io_locale/c_locale_mbstowcs.h"
---
+++ include/fast_io_locale/c_locale_mbstowcs.h
@@ -0,0 +1,51 @@
+#pragma once
+
+namespace fast_io
+{
+struct c_locale_multi_to_wide
+{
+	using manip_tag = manip_tag_t;
+	c_locale_observer cloc_ob;
+	char const *begin_ptr;
+	char const *end_ptr;
+	template<std::contiguous_iterator Iter>
+	requires std::same_as<std::iter_value_t<Iter>,char>
+	constexpr c_locale_multi_to_wide(c_locale_observer c,Iter begin,Iter end):
+		cloc_ob(c),begin_ptr(std::to_address(begin)),end_ptr(std::to_address(end)){}
+};
+namespace details
+{
+#if defined(__WINNT__) || defined(_MSC_VER)
+extern "C" errno_t __stdcall _mbstowcs_s_l(std::size_t*,wchar_t*,std::size_t,char const*,std::size_t,_locale_t);
+#endif
+
+template<typename... Args>
+requires (sizeof...(Args)==5)
+inline std::size_t c_mbstowcs_s_l(Args&& ...args)
+{
+#if defined(__WINNT__) || defined(_MSC_VER)
+	std::size_t sz{};
+	auto err{_mbstowcs_s_l(std::addressof(sz),std::forward<Args>(args)...)};
+	if(err)
+		throw std::system_error(static_cast<int>(err),std::generic_category());
+	return sz;
+#else
+	std::size_t sz{mbstowcs_l(std::forward<Args>(args)...)};
+	if(sz==std::numeric_limits<std::size_t>::max())
+		throw std::system_error(errno,std::generic_category());
+	return sz;
+#endif
+}
+}
+template<output_stream output>
+requires std::same_as<typename output::char_type,wchar_t>
+void print_define(output& out,c_locale_multi_to_wide ref)
+{
+	fast_io::internal_temporary_buffer<wchar_t> buffer;
+/*	for(;;)
+	{
+		details::c_mbstowcs_s_l(nullptr,)
+	}*/
+}
+
+}
