9bd3395efa7111880a9f8d567dababf87701c1f2
expnkx <euloanty@live.com>
2020-05-17 05:14:57 -0400

    fstream is shit

--- benchmarks/0000.10m_size_t/unit/fstream_native_locale.cc
+++ benchmarks/0000.10m_size_t/unit/fstream_native_locale.cc
@@ -2,17 +2,19 @@
 #include"../../../include/fast_io.h"
 #include"../../../include/fast_io_device.h"
 #include"../../../include/fast_io_legacy.h"
-#include"../../../include/fast_io_locale.h"
 
 int main()
 {
 	constexpr std::size_t N(10000000);
 	{
 	fast_io::timer t("output");
-	fast_io::c_locale loc(fast_io::c_locale_category::all);
-	fast_io::c_lconv_storage stg(loc);
-	fast_io::obuf_file obf("obuf_file_lcv_native.txt");
+	std::ofstream fout("fstream_native_locale.txt",std::ios::binary);
+	fout.imbue(std::locale(""));
+	auto& rdbuf(*fout.rdbuf());
 	for(std::size_t i{};i!=N;++i)
-		println(obf,fast_io::lcv(stg,i));
+	{
+		fout<<i;
+		rdbuf.sputc('\n');
+	}
 	}
 }
