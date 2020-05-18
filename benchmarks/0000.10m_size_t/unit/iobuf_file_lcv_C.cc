524314e7c905fc9ee1ed92ceac352ed137a82fdc
expnkx <euloanty@live.com>
2020-05-18 07:40:50 -0400

    locale

--- benchmarks/0000.10m_size_t/unit/iobuf_file_lcv_C.cc
+++ benchmarks/0000.10m_size_t/unit/iobuf_file_lcv_C.cc
@@ -7,19 +7,20 @@
 int main()
 {
 	constexpr std::size_t N(10000000);
-	{
-	fast_io::timer t("output");
 	fast_io::c_locale loc(fast_io::c_locale_category::all,"C");
+	{
 	fast_io::c_lconv_storage stg(loc);
+	fast_io::timer t("output");
 	fast_io::obuf_file obf("obuf_file_lcv_C.txt");
 	for(std::size_t i{};i!=N;++i)
 		println(obf,fast_io::lcv(stg,i));
 	}
 	std::vector<std::size_t> vec(N);
 	{
+	fast_io::c_ctype_isspace isspace(loc);
 	fast_io::timer t("input");
 	fast_io::ibuf_file ibf("obuf_file_lcv_C.txt");
 	for(std::size_t i{};i!=N;++i)
-		scan(ibf,vec[i]);
+		scan(ibf,fast_io::space(vec[i],isspace));
 	}
 }
