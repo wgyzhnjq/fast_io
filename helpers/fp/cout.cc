1e09f07f847d354015dd9b42c9d4f257f52f4ef4
expnkx <euloanty@live.com>
2020-05-22 05:18:18 -0400

    happy

---
+++ helpers/fp/cout.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+	double d;
+	std::cin>>d;
+	std::cout<<std::defaultfloat<<d<<'\n';
+	std::cout<<std::fixed<<d<<'\n';
+	std::cout<<std::scientific<<d<<'\n';
+	std::cout<<std::hexfloat<<d<<'\n';
+}
