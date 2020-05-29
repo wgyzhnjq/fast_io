6b16612c199ad8375f15b9cd297bf9f08c5dca2a
expnkx <euloanty@live.com>
2020-05-29 01:13:53 -0400

    provide a comparable benchmark between fast_io and boost_iostreams cqwrteur@DESKTOP-C4VAUFM:~$ ./boost_iostreams output: 0.43881950000000003s input:  0.5469978000000001s cqwrteur@DESKTOP-C4VAUFM:~$ ./iobuf_file output: 0.046920800000000006s input:  0.06757310000000001s Yes. fast_io defeats boost_iostreams for 10x. TEN TIMES OKAY???

---
+++ benchmarks/0000.10m_size_t/unit/boost_iostreams.cc
@@ -0,0 +1,33 @@
+#include"../../timer.h"
+#include"../../../include/fast_io.h"
+#include"../../../include/fast_io_device.h"
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include<ostream>
+#include<fstream>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	{
+	fast_io::timer t("output");
+	fast_io::posix_file pf("boost_fdstream.txt",fast_io::open_mode::binary|fast_io::open_mode::out);
+
+	boost::iostreams::stream_buffer<boost::iostreams::file_descriptor_sink> fdstreambuf
+	{boost::iostreams::file_descriptor_sink(pf.native_handle(),boost::iostreams::never_close_handle)};
+	std::ofstream out;
+	out.std::ostream::rdbuf(&fdstreambuf);
+	for(std::size_t i{};i!=N;++i)
+		out<<i<<'\n';
+	}
+	std::vector<std::size_t> vec(N);
+	{
+	fast_io::timer t("input");
+	fast_io::posix_file pf("boost_fdstream.txt",fast_io::open_mode::binary|fast_io::open_mode::in);
+	boost::iostreams::stream<boost::iostreams::file_descriptor_source> fdstream
+	(boost::iostreams::file_descriptor_source(pf.native_handle(),boost::iostreams::never_close_handle));
+	for(std::size_t i{};i!=N;++i)
+		fdstream>>vec[i];
+	}
+}
