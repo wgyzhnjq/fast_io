6738e9f849707f6ff20215215b0e88bb2be5499b
expnkx <euloanty@live.com>
2020-05-31 06:05:50 -0400

    remove constexpr
    It should be _base not base

--- include/fast_io_legacy_impl/c/freebsd.h
+++ include/fast_io_legacy_impl/c/freebsd.h
@@ -5,22 +5,22 @@
 namespace fast_io
 {
 
-inline constexpr char* obuffer_begin(c_io_observer_unlocked cio) noexcept
+inline char* obuffer_begin(c_io_observer_unlocked cio) noexcept
 {
-	return reinterpret_cast<char*>(cio.fp->_bf.base);
+	return reinterpret_cast<char*>(cio.fp->_bf._base);
 }
 
-inline constexpr char* obuffer_curr(c_io_observer_unlocked cio) noexcept
+inline char* obuffer_curr(c_io_observer_unlocked cio) noexcept
 {
 	return reinterpret_cast<char*>(cio.fp->_p);
 }
 
-inline constexpr char* obuffer_end(c_io_observer_unlocked cio) noexcept
+inline char* obuffer_end(c_io_observer_unlocked cio) noexcept
 {
 	return reinterpret_cast<char*>(cio.fp->_bf._p+cio.fp->_bf._w);
 }
 
-inline constexpr void obuffer_set_curr(c_io_observer_unlocked cio,char* ptr) noexcept
+inline void obuffer_set_curr(c_io_observer_unlocked cio,char* ptr) noexcept
 {
 	cio.fp->_w-=reinterpret_cast<char unsigned*>(ptr)-cio.fp->_p;
 	cio.fp->_p=reinterpret_cast<char unsigned*>(ptr);
