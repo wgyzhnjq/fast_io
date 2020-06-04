f218e66a440556b2251f3c38f1fed906bb5604eb
expnkx <euloanty@live.com>
2020-06-03 21:37:26 -0400

    add more unwrap

--- helpers/unwrap_std/znsosei.cc
+++ helpers/unwrap_std/znsosei.cc
@@ -11,6 +11,15 @@ inline std::ostream& wrapped_function(std::ostream& out,T v)
 	print(iob,v);
 	return out;
 }
+
+template<typename T>
+inline std::istream& wrapped_in_function(std::istream& in,T& v)
+{
+	fast_io::streambuf_io_observer iob{in.rdbuf()};
+	if(!scan<true>(iob,v))[[unlikely]]
+		in.setstate(std::ios_base::eofbit);
+	return in;
+}
 }
 
 extern "C" std::ostream& __wrap__ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x(std::ostream& out,char ch)
@@ -58,4 +67,46 @@ extern "C" std::ostream& __wrap__ZNSo9_M_insertIdEERSoT_(std::ostream& out,doubl
 extern "C" std::ostream& __wrap__ZNSo9_M_insertIeEERSoT_(std::ostream& out,long double v)
 {
 	return fast_io::wrapped_function(out,v);
+}
+
+
+
+extern "C" std::istream& __wrap__ZNSirsERi(std::istream& in,int& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIjEERSiRT_(std::istream& in,unsigned int& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIlEERSiRT_(std::istream& in,long& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractImEERSiRT_(std::istream& in,unsigned long& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIxEERSiRT_(std::istream& in,long long& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIyEERSiRT_(std::istream& in,unsigned long long& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIdEERSiRT_(std::istream& in,double& v)
+{
+	return fast_io::wrapped_in_function(in,v);
+}
+
+extern "C" std::istream& __wrap__ZNSi10_M_extractIfEERSiRT_(std::istream& in,float& v)
+{
+	return fast_io::wrapped_in_function(in,v);
 }
