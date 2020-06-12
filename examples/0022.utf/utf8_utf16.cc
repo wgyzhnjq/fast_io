14c9986149ba3cda80d31ae58a653aa6e893fa95
expnkx <euloanty@live.com>
2020-06-11 22:21:03 -0400

    utf8 utf16

--- examples/0022.utf/utf8_utf16.cc
+++ examples/0022.utf/utf8_utf16.cc
@@ -2,7 +2,12 @@
 
 int main()
 {
-	fast_io::wc_io_observer wstdout{stdout};
-	println(wstdout,fast_io::code_cvt("hello world\n"));
+	auto u16_str{fast_io::concat<std::u16string>(fast_io::code_cvt("hello world\n"))};
+	print_transaction(fast_io::out,[&](auto& out){
+		for(auto const& e : u16_str)
+		{
+			println(out,e);
+		}
+	});
 	print(fast_io::code_cvt(u"hello world\n"));
 }
