9ef74655946133777f0a69be0055d4ea0bf73830
expnkx <euloanty@live.com>
2020-06-11 22:11:38 -0400

    u8??

--- examples/0022.utf/utf8_utf16.cc
+++ examples/0022.utf/utf8_utf16.cc
@@ -4,5 +4,5 @@ int main()
 {
 	fast_io::wc_io_observer wstdout{stdout};
 	println(wstdout,fast_io::code_cvt("hello world\n"));
-	print(fast_io::code_cvt(L"hello world\n"));
+	print(fast_io::code_cvt(u"hello world\n"));
 }
