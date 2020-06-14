c9562d7050e601cd20084480b2e3c683f5ba8c61
expnkx <euloanty@live.com>
2020-06-14 19:54:52 -0400

    POSIX iconv support
    GB18030 example

---
+++ examples/0043.iconv/GB18030.cc
@@ -0,0 +1,26 @@
+#include"../../include/fast_io_locale/iconv.h"
+#include"../../include/fast_io.h"
+
+int main(int argc,char** argv)
+try
+{
+	if(argc!=3)
+	{
+		print(fast_io::err,"Usage: ",*argv," <source UTF8 file name> <dest GB18030 file name>\n");
+		return 1;
+	}
+	auto t0{std::chrono::high_resolution_clock::now()};
+	std::size_t transmitted{};
+	{
+	fast_io::posix_iconv picov("GB18030","UTF-8");
+	fast_io::basic_ibuf_posix_iconv_file<char,char8_t> utf8_file(picov,argv[1]);
+	fast_io::obuf_file file(argv[2]);
+	transmitted=transmit(file,utf8_file);
+	}
+	println(u8"UTF8 => GB18030 code cvt (with POSIX iconv system) success!\nTransmitted:",transmitted,u8" bytes\nElapsed Time:",std::chrono::high_resolution_clock::now()-t0);
+}
+catch(std::exception const& e)
+{
+	println(fast_io::err,e);
+	return 2;
+}
--- include/fast_io_locale/iconv.h
+++ include/fast_io_locale/iconv.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include"../fast_io_locale_device.h"
 #include<iconv.h>
 
 namespace fast_io
@@ -9,7 +10,7 @@ class posix_iconv_observer
 {
 public:
 	using native_handle_type = iconv_t;
-	iconv_t cd{(iconv_t) -1};
+	std::uintptr_t cd=-1;
 	constexpr decltype(auto) native_handle() const noexcept
 	{
 		return (cd);
@@ -18,10 +19,18 @@ public:
 	{
 		return (cd);
 	}
+	iconv_t get() noexcept
+	{
+		return bit_cast<iconv_t>(cd);
+	}
+	iconv_t get() const noexcept
+	{
+		return bit_cast<iconv_t>(cd);		
+	}
 	constexpr auto release() noexcept
 	{
 		auto temp{cd};
-		cd = (iconv_t) -1;
+		cd = -1;
 		return temp;
 	}
 };
@@ -31,28 +40,84 @@ class posix_iconv:public posix_iconv_observer
 public:
 	using native_handle_type = iconv_t;
 	constexpr posix_iconv()=default;
-	constexpr posix_iconv(iconv_t cd):posix_iconv_observer{cd}{}
-	posix_iconv(std::string_view to_code,std::string_view from_code):iconv_open(to_code.data(),from_code.data()){}
+	constexpr posix_iconv(std::uintptr_t cd):posix_iconv_observer{cd}{}
+	posix_iconv(std::string_view to_code,std::string_view from_code):posix_iconv(bit_cast<std::uintptr_t>(iconv_open(to_code.data(),from_code.data())))
+	{
+		if(this->native_handle()==std::uintptr_t(-1))
+#ifdef __cpp_exceptions
+			throw posix_error();
+#else
+			fast_terminate();
+#endif
+	}
 	posix_iconv(posix_iconv const&)=delete;
 	posix_iconv& operator=(posix_iconv const&)=delete;
 	constexpr posix_iconv(posix_iconv&& bmv) noexcept:posix_iconv_observer{bmv.native_handle()}
 	{
-		bmv.native_handle()=(iconv_t) -1;
+		bmv.native_handle()=-1;
 	}
 	posix_iconv& operator=(posix_iconv&& bmv) noexcept
 	{
 		if(std::addressof(bmv)==this)
 			return *this;
-		iconv_close(this->native_handle());
+		iconv_close(this->get());
 		this->native_handle()=bmv.native_handle();
-		bmv.native_handle()=(iconv_t) -1;
+		bmv.native_handle()=-1;
 		return *this;
 	}
 	~posix_iconv()
 	{
-		iconv_close(this->native_handle());
+		iconv_close(this->get());
 	}
 };
 
+class posix_iconv_function
+{
+public:
+	posix_iconv_observer iob{};
+	template<output_stream output,std::contiguous_iterator Iter>
+	Iter operator()(output& out,Iter begin,Iter end)
+	{
+		//we do not know how much it would require. Let's just put it as 8 times. It should be enough.
+		auto begin_ptr{reinterpret_cast<char*>(std::to_address(begin))};
+		std::size_t const buffer_size((end-begin)*8);
+		reserve_write(out,buffer_size,[&](auto ptr)
+		{
+			std::size_t insize(end-begin),avail(buffer_size);
+			auto ptr_ptr{reinterpret_cast<char*>(std::to_address(ptr))};
+			auto nconv{iconv(iob.get(),std::addressof(begin_ptr),std::addressof(insize),std::addressof(ptr_ptr),std::addressof(avail))};
+			if(nconv==std::numeric_limits<std::size_t>::max()&&errno!=EINVAL)
+			{
+#ifdef __cpp_exceptions
+				throw posix_error();
+#else
+				fast_termiante();
+#endif
+			}
+			return ptr+(ptr_ptr-reinterpret_cast<char*>(std::to_address(ptr)))/sizeof(*ptr);
+		});
+		return begin+(begin_ptr-reinterpret_cast<char*>(std::to_address(begin)))/sizeof(*begin);
+	}
+};
+
+template<refill_buffer_input_stream src,dynamic_buffer_output_stream indire>
+class basic_iconv:public basic_indirect_ibuffer<src,indire,posix_iconv_function>
+{
+public:
+	using source_buffer_type = src;
+	using indirect_buffer_type = indire;
+	using function_type = posix_iconv_function;
+	using char_type = typename indirect_buffer_type::char_type;
+	template<typename... Args>
+	requires std::constructible_from<source_buffer_type,Args...>
+	constexpr basic_iconv(posix_iconv_observer iob,Args&& ...args):basic_indirect_ibuffer<src,indire,function_type>{.source={std::forward<Args>(args)...},.function={iob}}
+	{}
+};
+
+template<std::integral new_code_type,input_stream input>
+using basic_ibuf_posix_iconv = basic_iconv<basic_ibuf<input>,vector_buffer<new_code_type>>;
+
+template<std::integral new_code_type,std::integral source_code_type>
+using basic_ibuf_posix_iconv_file = basic_ibuf_posix_iconv<new_code_type,basic_inative_file<source_code_type>>;
 
 }
