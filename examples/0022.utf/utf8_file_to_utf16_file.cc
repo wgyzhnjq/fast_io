d4945769ba512b8031fab481025b3ce9fe735686
expnkx <euloanty@live.com>
2020-06-14 18:53:02 -0400

    provide more information for utf8_file to utf16_file/utf32_file examples/0022.utf
    This can be an executable for doing your UTF8 to UTF16/UTF32 coding and it is at least 2.5x faster than iconv.

--- examples/0022.utf/utf8_file_to_utf16_file.cc
+++ examples/0022.utf/utf8_file_to_utf16_file.cc
@@ -9,10 +9,15 @@ try
 		print(fast_io::err,"Usage: ",*argv," <source UTF8 file name> <dest UTF16 (",std::endian::native==std::endian::little?"LE":"BE",") file name>\n");
 		return 1;
 	}
+	auto t0{std::chrono::high_resolution_clock::now()};
+	std::size_t transmitted{};
+	{
 	fast_io::ibuf_utf8_file<char16_t> utf8_file_in{argv[1]};
 	fast_io::basic_obuf_file<char16_t> utf16_file{argv[2]};
 	put(utf16_file,65279);	//write bom. 65279 is the code page for UTF-16 and UTF-32
-	transmit(utf16_file,utf8_file_in);
+	transmitted=transmit(utf16_file,utf8_file_in);
+	}
+	println(u8"UTF8 => UTF16 (",std::endian::native==std::endian::little?"LE":"BE",") code cvt success!\nTransmitted:",transmitted,u8" UTF16 code points\nElapsed Time:",std::chrono::high_resolution_clock::now()-t0);
 }
 catch(std::exception const& e)
 {
--- examples/0022.utf/utf8_file_to_utf32_file.cc
+++ examples/0022.utf/utf8_file_to_utf32_file.cc
@@ -9,10 +9,16 @@ try
 		print(fast_io::err,"Usage: ",*argv," <source UTF8 file name> <dest UTF32 (",std::endian::native==std::endian::little?"LE":"BE",") file name>\n");
 		return 1;
 	}
-	fast_io::ibuf_utf8_file<char32_t> utf8_file_in(argv[1]);
-	fast_io::basic_obuf_file<char32_t> u32_file(argv[2]);
-	put(u32_file,65279);	//write bom. 65279 is the code page for UTF-16 and UTF-32
-	transmit(u32_file,utf8_file_in);
+	auto t0{std::chrono::high_resolution_clock::now()};
+	std::size_t transmitted{};
+	{
+	fast_io::ibuf_utf8_file<char32_t> utf8_file_in{argv[1]};
+	fast_io::basic_obuf_file<char32_t> utf32_file{argv[2]};
+	put(utf32_file,65279);	//write bom. 65279 is the code page for UTF-16 and UTF-32
+	transmitted=transmit(utf32_file,utf8_file_in);
+	}
+	println(u8"UTF8 => UTF32 (",std::endian::native==std::endian::little?"LE":"BE",") code cvt success!\nTransmitted:",transmitted,u8" UTF32 code points\nElapsed Time:",std::chrono::high_resolution_clock::now()-t0);
+
 }
 catch(std::exception const& e)
 {
