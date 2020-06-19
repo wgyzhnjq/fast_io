21e4316af76d9c1c7f499f4bcfd86194816f3669
expnkx <euloanty@live.com>
2020-06-18 21:26:30 -0400

    add ovector to allow output to multiple output_streams

---
+++ include/fast_io_freestanding_impl/ovector.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace fast_io
+{
+
+template<output_stream output,typename vect_type = std::vector<typename output::char_type>>
+class basic_ovector:public vect_type
+{
+public:
+	using char_type = typename output::char_type;
+	using vector_type = vect_type;
+};
+
+template<output_stream output,typename vect_type = std::vector<typename output::char_type>,std::contiguous_iterator Iter>
+requires (std::same_as<typename output::char_type,std::iter_value_t<Iter>>&&std::same_as<typename output::char_type,char>)
+inline constexpr void write(basic_ovector<output,vec_type>& vec,Iter begin,Iter end)
+{
+	for(auto& e : vec)
+		write(e,begin,end);
+}
+
+}
