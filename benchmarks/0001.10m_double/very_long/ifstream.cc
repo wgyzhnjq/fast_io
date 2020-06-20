05ff693e6b657227cb9b65c8f8b5e96ce68fb86d
expnkx <euloanty@live.com>
2020-06-20 08:52:25 -0400

    find out a floating point bug

---
+++ benchmarks/0001.10m_double/very_long/ifstream.cc
@@ -0,0 +1,22 @@
+#include"../../timer.h"
+#include"../../../include/fast_io_device.h"
+#include"../../../include/fast_io.h"
+#include<fstream>
+
+int main()
+{
+	constexpr std::size_t N{2000000};
+	std::vector<double> buffer(N);
+	{
+		fast_io::timer tm("input");
+		std::ifstream fin("very_long.txt",std::ifstream::binary);
+		for(std::size_t i{};i!=N;++i)
+			fin>>buffer[i];
+	}
+	{
+		fast_io::timer t("output");
+		fast_io::obuf_file obf("ifstream.txt");
+		for(std::size_t i{};i!=N;++i)
+			println(obf,buffer[i]);
+	}
+}
