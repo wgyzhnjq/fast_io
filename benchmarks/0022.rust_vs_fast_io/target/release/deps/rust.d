0fdd58ecf6ef9ae0884e7e640fcc9e546a7af873
expnkx <euloanty@live.com>
2020-06-22 08:12:31 -0400

    remove dead stuffs

--- benchmarks/0022.rust_vs_fast_io/target/release/deps/rust.d
+++
@@ -1,5 +0,0 @@
-D:\hg\fast_io\benchmarks\0022.rust_vs_fast_io\target\release\deps\rust.exe: src\main.rs
-
-D:\hg\fast_io\benchmarks\0022.rust_vs_fast_io\target\release\deps\rust.d: src\main.rs
-
-src\main.rs:
--- benchmarks/0022.rust_vs_fast_io/target/release/rust.d
+++
@@ -1,1 +0,0 @@
-D:\hg\fast_io\benchmarks\0022.rust_vs_fast_io\target\release\rust.exe: D:\hg\fast_io\benchmarks\0022.rust_vs_fast_io\src\main.rs
