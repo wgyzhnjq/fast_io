ddabdb49f86fb0fb8a8817c9fe97cf7267d1ed03
expnkx <euloanty@live.com>
2020-06-27 20:19:09 -0400

    constant

---
+++ benchmarks/0023.constant/generate10k.cc
@@ -0,0 +1,27 @@
+#include"../../include/fast_io_device.h"
+
+int main()
+{
+	constexpr std::size_t N{100000};
+	{
+	fast_io::obuf_file obf("fast_io.cc");
+	print(obf,"#include\"../../include/fast_io.h\"\n\nint main()\n{\n\tstd::size_t a,b;\n\tscan(a,b);\n");
+	for(std::size_t i{};i!=N;++i)
+		print(obf,"\tprintln(a+b);\n");
+	put(obf,u8'}');
+	}
+	{
+	fast_io::obuf_file obf("fmt.cc");
+	print(obf,"#include<fmt/format.h>\n#include<cstdio>\nint main()\n{\n\tstd::size_t a,b;\n\tscanf(\"%zu%zu\",&a,&b);\n");
+	for(std::size_t i{};i!=N;++i)
+		print(obf,"\tfmt::print(stdout,\"{}\\n\",a+b);\n");
+	put(obf,u8'}');
+	}
+	{
+	fast_io::obuf_file obf("printf.cc");
+	print(obf,"#include<cstdio>\nint main()\n{\n\tstd::size_t a,b;\n\tscanf(\"%zu%zu\",&a,&b);\n");
+	for(std::size_t i{};i!=N;++i)
+		print(obf,"\tprintf(\"%d\\n\",a+b);\n");
+	put(obf,u8'}');
+	}
+}
