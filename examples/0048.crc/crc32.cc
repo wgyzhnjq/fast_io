5b4bf31b1b21878c500b80db858e388fc8aff917
expnkx <euloanty@live.com>
2020-06-29 16:14:40 -0400

    add non-intrinsic CRC32c. add CRC32 Extend the behavior of hash_processor to block size with 0

---
+++ examples/0048.crc/crc32.cc
@@ -0,0 +1,31 @@
+#include"../../include/fast_io.h"
+#include"../../include/fast_io_device.h"
+#include"../../include/fast_io_crypto.h"
+
+/*
+This is just for demo purpose.
+CRC is not a crypto hash algorithm. Do not use it for verification.
+*/
+
+
+int main(int argc,char** argv)
+try
+{
+	if(argc!=2)
+	{
+		perr("Usage: ",fast_io::chvw(*argv)," <file>\n");
+		return 1;
+	}
+	auto t0{std::chrono::high_resolution_clock::now()};
+	fast_io::crc32 c;
+	fast_io::hash_processor processor(c);
+	fast_io::ibuf_file ibf(argv[1]);
+	auto transmitted{transmit(processor,ibf)};
+	processor.do_final();
+	println(c," *",fast_io::chvw(argv[1]),"\nTransmitted:",transmitted,u8" bytes\tElapsed Time:",std::chrono::high_resolution_clock::now()-t0);
+}
+catch(std::exception const& e)
+{
+	perrln(e);
+	return 2;
+}
